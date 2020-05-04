c9ccfc794f5b5500f41f4be8d7096735308b34c7
Maciej Kowalczyk <mackow3@st.amu.edu.pl>
2020-05-04 19:58:18 +0200

    Styling for add new note

--- App/Screens/Functionalities/AddNote.js
+++ App/Screens/Functionalities/AddNote.js
@@ -1,13 +1,83 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
-import {ScrollView, Text} from 'react-native';
+import React, { useState, useCallback } from 'react';
+import {ScrollView, Text, StyleSheet, View, TextInput, TouchableOpacity} from 'react-native';
+import {useDispatch} from 'react-redux';
 
 const AddNote = (props) => {
+    const dispatch = useDispatch();
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    const submitHandler = useCallback(() => {
+      }, []);
+
   return (
-    <ScrollView style={{backgroundColor: '#222222', flex: 1}}>
-      <Text>Add Note</Text>
-    </ScrollView>
+    <ScrollView style={styles.form}>
+      <View style={{paddingVertical: 15}}>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.input}
+          value={title}
+          onChangeText={(text) => setTitle(text)}
+        />
+      </View>
+      <View style={{paddingVertical: 15}}>
+        <Text style={styles.label}>Description</Text>
+        <TextInput
+          style={styles.input}
+          value={description}
+          onChangeText={(text) => setDescription(text)}
+        />
+      </View>
+      <View style={{alignItems: 'center', margin: 20}}>
+        <TouchableOpacity style={styles.button} onPress={submitHandler}>
+          <Text style={styles.buttonText}>Submit</Text>
+        </TouchableOpacity>
+      </View>
+      </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+    form: {
+      backgroundColor: '#222222',
+      flex: 1,
+    },
+    label: {
+      fontFamily: 'open-sans-bold',
+      marginRight: '10%',
+      marginLeft: '10%',
+      marginTop: '5%',
+      color: '#FF8C00',
+      fontSize: 20,
+      fontWeight: 'bold',
+    },
+    input: {
+      color: '#FFFFFF',
+      fontSize: 20,
+      paddingHorizontal: 2,
+      paddingVertical: 5,
+      borderBottomColor: '#FFA500',
+      borderBottomWidth: 1,
+      marginVertical: 2,
+      marginHorizontal: 2,
+      marginLeft: '10%',
+      marginRight: '10%',
+    },
+    button: {
+      borderRadius: 10,
+      backgroundColor: '#FF8C00',
+      alignItems: 'center',
+      width: '40%',
+      padding: 15,
+      margin: 10,
+    },
+    buttonText: {
+      fontWeight: 'bold',
+      fontSize: 16,
+      color: '#FFFFFF',
+    },
+  });
+  
 
 export default AddNote;
