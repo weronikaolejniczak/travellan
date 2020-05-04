909fd2e2bc2e1ed29027c9574f9dfd682ef676d6
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-05-04 22:33:52 +0200

    show proper coordinates

--- App/Data/DummyData.js
+++ App/Data/DummyData.js
@@ -2,10 +2,10 @@ import Trip from '../Models/TripModel';
 
 /**
  * Dummy trips
- *  [int]               id, 
-    [int]               ownerId, 
-    [string]            destination, 
-    [string]            imageUrl, 
+ *  [int]               id,
+    [int]               ownerId,
+    [string]            destination,
+    [string]            imageUrl,
     [date]              startDate,
     [date]              endDate
     [float]             budget,
@@ -13,7 +13,7 @@ import Trip from '../Models/TripModel';
     [dict]              transportInfo,
     [dict]              accommodationInfo,
     [dict of dict]      pointsOfInterest
- * 
+ *
  */
 
 const TRIPS = [
@@ -22,7 +22,12 @@ const TRIPS = [
     1,
     1,
     'Barcelona',
-    {},
+    {
+      latitude: 41.385063,
+      longitude: 2.173404,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    },
     'https://images.unsplash.com/photo-1562883676-8c7feb83f09b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=661&q=80',
     '2021-1-21',
     '2021-1-25',
@@ -37,7 +42,12 @@ const TRIPS = [
     2,
     1,
     'Paris',
-    {},
+    {
+      latitude: 48.864716,
+      longitude: 2.349014,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    },
     'https://images.unsplash.com/photo-1502602898657-3e91760cbb34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1052&q=80',
     '2021-2-13',
     '2021-2-15',
@@ -52,7 +62,12 @@ const TRIPS = [
     3,
     1,
     'Tokyo',
-    {},
+    {
+      latitude: 35.689487,
+      longitude: 139.691711,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    },
     'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60',
     '2021-4-10',
     '2021-4-21',
@@ -67,7 +82,12 @@ const TRIPS = [
     4,
     1,
     'Warsaw',
-    {},
+    {
+      latitude: 52.229675,
+      longitude: 21.01223,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    },
     'https://images.unsplash.com/photo-1519197924294-4ba991a11128?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60',
     '2021-8-27',
     '2021-8-30',
@@ -82,7 +102,12 @@ const TRIPS = [
     5,
     1,
     'Milan',
-    {},
+    {
+      latitude: 45.464203,
+      longitude: 9.189982,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    },
     'https://images.unsplash.com/photo-1520440229-6469a149ac59?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60',
     '2021-11-12',
     '2021-11-20',
