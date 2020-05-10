45f39afbab991f30ea892e314f5c8dc31a8a7724
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-05-10 22:54:39 +0200

    weatherScreenStyle

---
+++ App/Screens/Weather/WeatherScreenStyle.js
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+import Colors from '../../Constants/Colors';
+
+export const weatherScreenStyle = StyleSheet.create({
+  contentContainer: {
+    backgroundColor: Colors.background,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: Colors.text,
+  },
+});
