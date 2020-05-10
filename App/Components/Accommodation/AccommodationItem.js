42cd5a829bb8936817a87204745c38f35534fc9e
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-05-10 23:28:17 +0200

    accommodationItemStyle

--- App/Components/Accommodation/AccommodationItem.js
+++ App/Components/Accommodation/AccommodationItem.js
@@ -1,32 +1,17 @@
 import React from 'react';
-import {
-  View,
-  ScrollView,
-  Text,
-  TouchableOpacity,
-  Alert,
-  StyleSheet,
-  Dimensions,
-} from 'react-native';
+import {View, ScrollView, Text, TouchableOpacity, Alert} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
-/**
- * IMPORTS FROM WITHIN THE MODULE
- */
+/** IMPORTS FROM WITHIN THE MODULE */
 import Card from '../../Components/UI/Card';
 import ReadMore from '../../Components/UI/ReadMore';
-import Colors from '../../Constants/Colors';
-
-// refactor Metrics
-const {height, width} = Dimensions.get('window');
-export const cardHeight = height * 0.82;
-export const cardWidth = width * 0.82;
-export const spacingForCardInset = width * 0.1 - 13;
+import {accommodationItemStyle as styles} from './AccommodationItemStyle';
 
 /**
- * Accommodation item component used in AccommodationScreen for reservations listing.
+ * Accommodation item component used in AccommodationScreen for reservations listing
  * TODO:
  * refactor icons for better touchable response and clickability
  * refactor action bar
+ * refactor inline styles
  */
 const AccommodationItem = (props) => {
   return (
@@ -70,68 +55,5 @@ const AccommodationItem = (props) => {
     </Card>
   );
 };
-
-/**
- * TODO:
- * refactor Colors
- * refactor Fonts
- * refactor Metrics
- * refactor into seperate file AccommodationItemStyle
- */
-const styles = StyleSheet.create({
-  accommodation: {
-    width: cardWidth,
-    height: cardHeight,
-    borderRadius: 15,
-    marginHorizontal: width * 0.025,
-    paddingTop: height * 0.101,
-    paddingBottom: height * 0.02,
-    paddingHorizontal: width * 0.03,
-  },
-  // refactor triple Colors.text
-  header: {
-    color: Colors.text,
-    fontSize: 24,
-  },
-  h2: {
-    color: Colors.text,
-    fontSize: 18,
-  },
-  subtitle: {
-    color: Colors.text,
-    fontSize: 20,
-  },
-  text: {
-    fontSize: 16,
-    color: Colors.text,
-  },
-  textAlign: {
-    textAlign: 'justify',
-  },
-  imageContainer: {
-    width: '100%',
-    height: 150,
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-  },
-  actions: {
-    flexDirection: 'row',
-    padding: 15,
-    backgroundColor: Colors.primary,
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-  },
-  icon: {
-    fontSize: 30,
-    color: Colors.text,
-    marginRight: 30,
-  },
-});
 
 export default AccommodationItem;
---
+++ App/Components/Accommodation/AccommodationItemStyle.js
@@ -0,0 +1,59 @@
+import {StyleSheet, Dimensions} from 'react-native';
+import Colors from '../../Constants/Colors';
+
+const {height, width} = Dimensions.get('window');
+export const cardHeight = height * 0.82;
+export const cardWidth = width * 0.82;
+export const spacingForCardInset = width * 0.1 - 13;
+
+/**
+ * TODO:
+ * refactor Fonts
+ * refactor Metrics
+ */
+export const accommodationItemStyle = StyleSheet.create({
+  accommodation: {
+    width: cardWidth,
+    height: cardHeight,
+    borderRadius: 15,
+    marginHorizontal: width * 0.025,
+    paddingTop: height * 0.101,
+    paddingBottom: height * 0.02,
+    paddingHorizontal: width * 0.03,
+  },
+  header: {
+    color: Colors.text,
+    fontSize: 24,
+  },
+  h2: {
+    color: Colors.text,
+    fontSize: 18,
+  },
+  subtitle: {
+    color: Colors.text,
+    fontSize: 20,
+  },
+  text: {
+    fontSize: 16,
+    color: Colors.text,
+  },
+  textAlign: {
+    textAlign: 'justify',
+  },
+  actions: {
+    flexDirection: 'row',
+    padding: 15,
+    backgroundColor: Colors.primary,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+  },
+  icon: {
+    fontSize: 30,
+    color: Colors.text,
+    marginRight: 30,
+  },
+});
