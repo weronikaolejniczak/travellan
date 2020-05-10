07833ee32bf71e3d58efbbc899f84551bf4d848f
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-05-10 23:19:08 +0200

    refactor navigation

--- App/App.js
+++ App/App.js
@@ -1,40 +1,11 @@
-import 'react-native-gesture-handler';
 import React from 'react';
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
 import thunk from 'redux-thunk';
-/**
- * Imports from within the module
- */
-// reducers
+/** IMPORTS FROM WITHIN THE MODULE */
 import tripsReducer from './Stores/Reducers/Trips';
 import notesReducer from './Stores/Reducers/Notes';
-// constants
-import Colors from './Constants/Colors';
-// trips overview screen
-import TripsOverviewScreen, {
-  tripsScreenOptions,
-} from './Screens/MyTrips/TripsOverviewScreen';
-import TripDetailScreen, {
-  tripDetailScreenOptions,
-} from './Screens/MyTrips/TripDetailScreen';
-// 'create a new trip' screen
-import NewTripScreen from './Screens/MyTrips/NewTripScreen';
-// functionalities screens
-import TransportScreen, {
-  transportScreenOptions,
-} from './Screens/Transport/TransportScreen';
-import AccommodationScreen, {
-  accommodationScreenOptions,
-} from './Screens/Accommodation/AccommodationScreen';
-import MapScreen from './Screens/Map/MapScreen';
-import DailyPlanScreen from './Screens/DailyPlan/DailyPlanScreen';
-import WeatherScreen from './Screens/Weather/WeatherScreen';
-import BudgetScreen from './Screens/Budget/BudgetScreen';
-import NotesScreen from './Screens/Notes/NotesScreen';
-import AddNote from './Screens/Notes/AddNoteScreen';
+import Navigation from './Services/NavigationService';
 
 // refactor combineReducers to be elsewhere
 const rootReducer = combineReducers({
@@ -44,59 +15,12 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
-// stack navigator
-const Stack = createStackNavigator();
-
-// default navigation options
-const defaultNavOptions = {
-  headerStyle: {
-    backgroundColor: Colors.primary,
-  },
-  headerTintColor: '#fff',
-  headerTitleStyle: {
-    fontWeight: 'bold',
-  },
-};
-
-/**
- * Main application function
- * with navigation;
- * REFACTOR
+/** Main application function
  */
 export default function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={defaultNavOptions}>
-          <Stack.Screen
-            name="My trips"
-            component={TripsOverviewScreen}
-            options={tripsScreenOptions}
-          />
-          <Stack.Screen name="Create a trip" component={NewTripScreen} />
-          <Stack.Screen
-            name="Details"
-            component={TripDetailScreen}
-            options={tripDetailScreenOptions}
-          />
-          <Stack.Screen
-            name="Transport"
-            component={TransportScreen}
-            options={transportScreenOptions}
-          />
-          <Stack.Screen
-            name="Housing"
-            component={AccommodationScreen}
-            options={accommodationScreenOptions}
-          />
-          <Stack.Screen name="Map" component={MapScreen} />
-          <Stack.Screen name="Daily plan" component={DailyPlanScreen} />
-          <Stack.Screen name="Weather" component={WeatherScreen} />
-          <Stack.Screen name="Budget" component={BudgetScreen} />
-          <Stack.Screen name="Notes" component={NotesScreen} />
-          <Stack.Screen name="Add Note" component={AddNote} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Navigation />
     </Provider>
   );
 }
---
+++ App/Services/NavigationService.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+/** SCREENS */
+import TripsOverviewScreen, {
+  tripsScreenOptions,
+} from '../Screens/MyTrips/TripsOverviewScreen';
+import TripDetailScreen, {
+  tripDetailScreenOptions,
+} from '../Screens/MyTrips/TripDetailScreen';
+import NewTripScreen from '../Screens/MyTrips/NewTripScreen';
+import TransportScreen, {
+  transportScreenOptions,
+} from '../Screens/Transport/TransportScreen';
+import AccommodationScreen, {
+  accommodationScreenOptions,
+} from '../Screens/Accommodation/AccommodationScreen';
+import MapScreen from '../Screens/Map/MapScreen';
+import DailyPlanScreen from '../Screens/DailyPlan/DailyPlanScreen';
+import WeatherScreen from '../Screens/Weather/WeatherScreen';
+import BudgetScreen from '../Screens/Budget/BudgetScreen';
+import NotesScreen from '../Screens/Notes/NotesScreen';
+import AddNote from '../Screens/Notes/AddNoteScreen';
+/** CONSTANTS */
+import Colors from '../Constants/Colors';
+
+/** STACK NAVIGATOR */
+const Stack = createStackNavigator();
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={defaultNavOptions}>
+        <Stack.Screen
+          name="My trips"
+          component={TripsOverviewScreen}
+          options={tripsScreenOptions}
+        />
+        <Stack.Screen name="Create a trip" component={NewTripScreen} />
+        <Stack.Screen
+          name="Details"
+          component={TripDetailScreen}
+          options={tripDetailScreenOptions}
+        />
+        <Stack.Screen
+          name="Transport"
+          component={TransportScreen}
+          options={transportScreenOptions}
+        />
+        <Stack.Screen
+          name="Housing"
+          component={AccommodationScreen}
+          options={accommodationScreenOptions}
+        />
+        <Stack.Screen name="Map" component={MapScreen} />
+        <Stack.Screen name="Daily plan" component={DailyPlanScreen} />
+        <Stack.Screen name="Weather" component={WeatherScreen} />
+        <Stack.Screen name="Budget" component={BudgetScreen} />
+        <Stack.Screen name="Notes" component={NotesScreen} />
+        <Stack.Screen name="Add Note" component={AddNote} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+/** DEFAULT NAVIGATION OPTIONS */
+const defaultNavOptions = {
+  headerStyle: {
+    backgroundColor: Colors.primary,
+  },
+  headerTintColor: Colors.text,
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
+};
