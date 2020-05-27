7a5324e3f9f8e0bf704b66c8c2d07bdbb1a1fe76
Maciej Kowalczyk <mackow3@st.amu.edu.pl>
2020-05-27 13:01:34 +0200

    Adding notes

--- App/Stores/index.js
+++ App/Stores/index.js
@@ -2,11 +2,9 @@ import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 /** REDUCERS */
 import tripsReducer from '../Stores/Reducers/Trips';
-import notesReducer from '../Stores/Reducers/Notes';
 
 const rootReducer = combineReducers({
   trips: tripsReducer,
-  notes: notesReducer,
 });
 
 export const store = createStore(rootReducer, applyMiddleware(thunk));
