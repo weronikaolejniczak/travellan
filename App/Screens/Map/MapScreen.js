dd1b57bbd75b5a48238722ae8ed416a5a4b50c93
Maciej Kowalczyk <mackow3@st.amu.edu.pl>
2020-06-01 20:15:09 +0200

    geolocation config, marker+title on destination

--- App/Screens/Map/MapScreen.js
+++ App/Screens/Map/MapScreen.js
@@ -1,11 +1,18 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, { Component } from 'react';
 import {useSelector} from 'react-redux';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 // imports from within the module
 import {mapStyle} from './MapScreenStyle';
+import Geolocation from '@react-native-community/geolocation';
+import {request, PERMISSIONS} from 'react-native-permissions';
+import {
+  Platform
+} from 'react-native';
+
 
 const MapScreen = (props) => {
+
   const tripId = props.route.params.tripId;
   const selectedTrip = useSelector((state) =>
     state.trips.availableTrips.find((item) => item.id === tripId),
@@ -15,14 +22,24 @@ const MapScreen = (props) => {
     return selectedTrip.region;
   };
 
+  
   return (
     <MapView
       provider={PROVIDER_GOOGLE}
       style={{flex: 1}}
       customMapStyle={mapStyle}
       initialRegion={extractRegion()}
-    />
+      showsUserLocation={true}
+    >
+      <Marker
+      coordinate={extractRegion()}
+      title={selectedTrip.destination}
+      >
+      </Marker>
+
+    </MapView>
   );
+
 };
 
 export default MapScreen;
