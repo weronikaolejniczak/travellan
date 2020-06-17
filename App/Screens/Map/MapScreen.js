953c5e2bc96bd4df95c1c896d0bc700898d59b4e
Maciej Kowalczyk <mackow3@st.amu.edu.pl>
2020-06-17 19:53:46 +0200

    placing markers by clicking on the map

--- App/Screens/Map/MapScreen.js
+++ App/Screens/Map/MapScreen.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {useSelector, setState} from 'react-redux';
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 // imports from within the module
@@ -7,7 +7,7 @@ import {mapStyle} from './MapScreenStyle';
 import Geolocation from '@react-native-community/geolocation';
 import {request, PERMISSIONS} from 'react-native-permissions';
 import {
-  Platform}
+  Platform, Button, View}
 from 'react-native';
 
 
@@ -21,8 +21,35 @@ const MapScreen = (props) => {
   const extractRegion = () => {
     return selectedTrip.region;
   };
-  
 
+  const [currentPosition, setCurrentPosition] = useState(selectedTrip.region)
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(position=>{
+      //console.log(JSON.stringify(position))
+      const { longitude, latitude} = position.coords
+      setCurrentPosition({
+        ...currentPosition,
+        longitude,
+        latitude,
+      })
+    }, error => alert(error.message),
+    {timeout: 20000, maximumAge: 1000})
+
+    
+  }, [])
+
+  const [marker, setNewMarker]= useState(selectedTrip.region)
+
+  const getMarkerCoords = (coords) => {
+    const { longitude, latitude }= coords.nativeEvent.coordinate
+    setNewMarker({
+      ...marker,
+      longitude,
+      latitude,
+    })
+  };
+  
   
   return (
     <MapView
@@ -31,14 +58,15 @@ const MapScreen = (props) => {
       customMapStyle={mapStyle}
       initialRegion={extractRegion()}
       showsUserLocation={true}
-
+      showsMyLocationButton={true}
+      onPress={ (event) => getMarkerCoords(event)}
     >
       <Marker
-      coordinate={extractRegion()}
-      title={selectedTrip.destination}
-      >
+      coordinate={marker}>
       </Marker>
+      
     </MapView>
+    
   );
 
 };
