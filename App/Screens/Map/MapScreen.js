cbb8f6b75ddc4bd36dcf1b41e06e19fdbbc529da
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-06-23 22:05:44 +0200

    clean map screen

--- App/Screens/Map/MapScreen.js
+++ App/Screens/Map/MapScreen.js
@@ -1,18 +1,14 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import {useSelector, setState} from 'react-redux';
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 // imports from within the module
 import {mapStyle} from './MapScreenStyle';
 import Geolocation from '@react-native-community/geolocation';
-import {request, PERMISSIONS} from 'react-native-permissions';
-import {
-  Platform, Button, View}
-from 'react-native';
-
+/* import {request, PERMISSIONS} from 'react-native-permissions';
+import {Platform, Button, View} from 'react-native'; */
 
 const MapScreen = (props) => {
-
   const tripId = props.route.params.tripId;
   const selectedTrip = useSelector((state) =>
     state.trips.availableTrips.find((item) => item.id === tripId),
@@ -22,35 +18,35 @@ const MapScreen = (props) => {
     return selectedTrip.region;
   };
 
-  const [currentPosition, setCurrentPosition] = useState(selectedTrip.region)
+  const [currentPosition, setCurrentPosition] = useState(selectedTrip.region);
 
   useEffect(() => {
-    Geolocation.getCurrentPosition(position=>{
-      //console.log(JSON.stringify(position))
-      const { longitude, latitude} = position.coords
-      setCurrentPosition({
-        ...currentPosition,
-        longitude,
-        latitude,
-      })
-    }, error => alert(error.message),
-    {timeout: 20000, maximumAge: 1000})
+    Geolocation.getCurrentPosition(
+      (position) => {
+        //console.log(JSON.stringify(position))
+        const {longitude, latitude} = position.coords;
+        setCurrentPosition({
+          ...currentPosition,
+          longitude,
+          latitude,
+        });
+      },
+      (error) => alert(error.message),
+      {timeout: 20000, maximumAge: 1000},
+    );
+  }, [currentPosition]);
 
-    
-  }, [])
-
-  const [marker, setNewMarker]= useState(selectedTrip.region)
+  const [marker, setNewMarker] = useState(selectedTrip.region);
 
   const getMarkerCoords = (coords) => {
-    const { longitude, latitude }= coords.nativeEvent.coordinate
+    const {longitude, latitude} = coords.nativeEvent.coordinate;
     setNewMarker({
       ...marker,
       longitude,
       latitude,
-    })
+    });
   };
-  
-  
+
   return (
     <MapView
       provider={PROVIDER_GOOGLE}
@@ -59,15 +55,11 @@ const MapScreen = (props) => {
       initialRegion={extractRegion()}
       showsUserLocation={true}
       showsMyLocationButton={true}
-      onPress={ (event) => getMarkerCoords(event)}
-    >
-      <MapView.Marker coordinate={extractRegion()}/>
-      <MapView.Marker coordinate={marker}/>
-      
+      onPress={(event) => getMarkerCoords(event)}>
+      <MapView.Marker coordinate={extractRegion()} />
+      <MapView.Marker coordinate={marker} />
     </MapView>
-    
   );
-
 };
 
 export default MapScreen;
