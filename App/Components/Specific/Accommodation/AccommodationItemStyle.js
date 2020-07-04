dff455c3c2e37d1127483eb7baaa0d037773e5c4
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-07-05 00:10:08 +0200

    implement action bar in map screen

--- App/Components/Specific/Accommodation/AccommodationItemStyle.js
+++ App/Components/Specific/Accommodation/AccommodationItemStyle.js
@@ -3,7 +3,7 @@ import Colors from '../../../Constants/Colors';
 
 const {height, width} = Dimensions.get('window');
 export const cardHeight = height * 0.83;
-export const cardWidth = width * 0.925;
+export const cardWidth = width * 0.923;
 export const spacingForCardInset = width * 0.03;
 
 /**
--- App/Screens/Map/MapScreen.js
+++ App/Screens/Map/MapScreen.js
@@ -1,25 +1,39 @@
-/* eslint-disable react-native/no-inline-styles */
 import React, {useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
 import {useSelector, setState} from 'react-redux';
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
-// imports from within the module
-import {mapStyle} from './MapScreenStyle';
 import Geolocation from '@react-native-community/geolocation';
-/* import {request, PERMISSIONS} from 'react-native-permissions';
-import {Platform, Button, View} from 'react-native'; */
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+/* import {request, PERMISSIONS} from 'react-native-permissions'; */
+/* IMPORTS FROM WITHIN THE MODULE */
+import {mapStyle} from './MapScreenStyle';
+import Colors from '../../Constants/Colors';
 
 const MapScreen = (props) => {
   const tripId = props.route.params.tripId;
   const selectedTrip = useSelector((state) =>
     state.trips.availableTrips.find((item) => item.id === tripId),
   );
-
   const extractRegion = () => {
     return selectedTrip.region;
   };
 
+  /** STATE VARIABLES AND STATE SETTER FUNCTIONS */
   const [currentPosition, setCurrentPosition] = useState(selectedTrip.region);
+  const [marker, setNewMarker] = useState(selectedTrip.region);
+  const [addingMarkerActive, setAddingMarkerActive] = useState(false);
+  const [deletingMarkerActive, setDeletingMarkerActive] = useState(false);
+  const [mapSearchActive, setMapSearchActive] = useState(false);
+  const [placeToSearch, setPlaceToSearch] = useState('');
 
+  /** HANDLERS */
   useEffect(() => {
     Geolocation.getCurrentPosition(
       (position) => {
@@ -36,7 +50,29 @@ const MapScreen = (props) => {
     );
   }, [currentPosition]);
 
-  const [marker, setNewMarker] = useState(selectedTrip.region);
+  const searchActivityHandler = () => {
+    if (!mapSearchActive) {
+      setAddingMarkerActive(false);
+      setDeletingMarkerActive(false);
+    }
+    setMapSearchActive(!mapSearchActive);
+  };
+
+  const addingActivityHandler = () => {
+    if (!addingMarkerActive) {
+      setDeletingMarkerActive(false);
+      setMapSearchActive(false);
+    }
+    setAddingMarkerActive(!addingMarkerActive);
+  };
+
+  const deletingActivityHandler = () => {
+    if (!deletingMarkerActive) {
+      setAddingMarkerActive(false);
+      setMapSearchActive(false);
+    }
+    setDeletingMarkerActive(!deletingMarkerActive);
+  };
 
   const getMarkerCoords = (coords) => {
     const {longitude, latitude} = coords.nativeEvent.coordinate;
@@ -48,16 +84,135 @@ const MapScreen = (props) => {
   };
 
   return (
-    <MapView
-      provider={PROVIDER_GOOGLE}
-      style={{flex: 1}}
-      customMapStyle={mapStyle}
-      initialRegion={extractRegion()}
-      showsUserLocation={true}
-      showsMyLocationButton={true}
-      onPress={(event) => getMarkerCoords(event)}>
-    </MapView>
+    <View style={styles.flex}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.flex}
+        customMapStyle={mapStyle}
+        initialRegion={extractRegion()}
+        showsUserLocation={true}
+        showsMyLocationButton={true}
+        onPress={(event) => getMarkerCoords(event)}
+      />
+      <View style={styles.overlay}>
+        <View style={styles.actionBar}>
+          {/* ADD MARKER */}
+          <View
+            style={{
+              backgroundColor: addingMarkerActive
+                ? Colors.background
+                : Colors.cards,
+            }}>
+            <TouchableOpacity
+              styles={styles.button}
+              onPress={addingActivityHandler}>
+              <Icon name="map-marker-plus" style={styles.icon} />
+            </TouchableOpacity>
+          </View>
+          {/* DELETE MARKER */}
+          <View
+            style={{
+              backgroundColor: deletingMarkerActive
+                ? Colors.background
+                : Colors.cards,
+            }}>
+            <TouchableOpacity
+              styles={styles.button}
+              onPress={deletingActivityHandler}>
+              <Icon name="map-marker-minus" style={styles.icon} />
+            </TouchableOpacity>
+          </View>
+          {/* SEE A ROUTE BETWEEN TWO MARKERS */}
+          {/* <View style={{}}>
+            <TouchableOpacity styles={styles.button}>
+              <Icon name="map-marker-path" style={styles.icon} />
+            </TouchableOpacity>
+          </View>*/}
+          {/* SEARCH FOR PLACE */}
+          <View
+            style={{
+              backgroundColor: mapSearchActive
+                ? Colors.background
+                : Colors.cards,
+            }}>
+            <TouchableOpacity
+              styles={styles.button}
+              onPress={searchActivityHandler}>
+              <Icon name="map-search" style={styles.icon} />
+            </TouchableOpacity>
+          </View>
+        </View>
+        {/** SEARCH BAR */}
+        <View style={{alignItems: 'center'}}>
+          {mapSearchActive && (
+            <View style={styles.searchBar}>
+              <TextInput
+                placeholder="Address or name of place"
+                placeholderTextColor={'grey'}
+                style={styles.input}
+                onChangeText={(text) => setPlaceToSearch(text)}
+                value={placeToSearch}
+              />
+              <View style={{position: 'absolute', right: 0}}>
+                <TouchableOpacity styles={styles.button}>
+                  <MaterialIcon name="search" style={styles.icon} />
+                </TouchableOpacity>
+              </View>
+            </View>
+          )}
+        </View>
+      </View>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  flex: {
+    flex: 1,
+  },
+  overlay: {
+    top: 0,
+    position: 'absolute',
+    width: '100%',
+    backgroundColor: Colors.cards,
+  },
+  actionBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  searchBar: {
+    width: '100%',
+    justifyContent: 'center',
+    backgroundColor: Colors.cards,
+  },
+  input: {
+    width: '100%',
+    padding: 15,
+    paddingLeft: 20,
+    borderTopWidth: 1,
+    borderColor: Colors.text,
+    fontSize: 14,
+    color: Colors.text,
+  },
+  button: {
+    backgroundColor: Colors.text,
+    padding: 20,
+  },
+  icon: {
+    padding: 15,
+    fontSize: 28,
+    color: Colors.text,
+  },
+  text: {
+    color: Colors.text,
+  },
+});
 
 export default MapScreen;
