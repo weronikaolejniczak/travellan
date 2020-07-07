bac26d8e34a27aed873a891b523d8c9785c83c9f
Shanhavael <konrad.skrzypczak1@op.pl>
2020-07-07 14:14:21 +0200

    add currency screen layout

--- App/Screens/Budget/AddCurrencyScreen.js
+++ App/Screens/Budget/AddCurrencyScreen.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import {ScrollView, Text} from 'react-native';
+import {View, Text, TouchableOpacity, TextInput} from 'react-native';
+/** IMPORTS FROM WITHIN THE MODULE */
+import {AddCurrencyScreenStyles as styles} from './AddCurrencyScreenStyle';
 
 const AddCurrency = (props) => {
   return (
-    <ScrollView>
-      <Text>Hey</Text>
-    </ScrollView>
+    <View style={styles.container}>
+      <View style={styles.elementContainer}>
+        <Text style={styles.label}>Initial value</Text>
+        <TextInput
+          placeholder="Amount"
+          placeholderTextColor="grey"
+          style={styles.input}
+          keyboardType={'numeric'}
+        />
+      </View>
+      <View style={styles.elementContainer}>
+        <Text style={styles.label}>Currency</Text>
+        {/* PICKER */}
+      </View>
+      <View style={styles.elementContainer}>
+        <View style={{alignItems: 'center', margin: 20}}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Submit</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
   );
 };
 
