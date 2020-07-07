ff5f0d9cbb74254c2722f5627701b728799e217a
Shanhavael <konrad.skrzypczak1@op.pl>
2020-07-07 13:48:22 +0200

    add currency screen and add currency screen style

---
+++ App/Screens/Budget/AddCurrencyScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {ScrollView, Text} from 'react-native';
+
+const AddNote = (props) => {
+  return (
+    <ScrollView>
+      <Text>Hey</Text>
+    </ScrollView>
+  );
+};
+
+export default AddNote;
