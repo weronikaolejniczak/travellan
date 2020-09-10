e83314af9e825b65a3f2ab3aba87247755bc628f
CalderBriar <cethan@interia.pl>
2020-09-10 03:11:56 +0200

    clear + added button under QR

--- src/domains/transport/containers/AddQR.js
+++ src/domains/transport/containers/AddQR.js
@@ -6,7 +6,7 @@ import {
   //AppRegistry,
   //StyleSheet,
   Text,
-  //TouchableOpacity,
+  TouchableOpacity,
   //Linking,
 } from 'react-native';
 
@@ -18,46 +18,28 @@ import {AddQRStyle as styles} from './AddQRStyle';
 import {useDispatch, useSelector} from 'react-redux';
 
 import * as transportActions from 'transport/state/Actions';
-/*
-class ScanScreen extends Component {
-  onSuccess = e => {
-    Linking.openURL(e.data).catch(err =>
-      console.error('An error occured', err)
-    );
-  };
-*/
-//class AddQR extends Component {
+
 const AddQR = (props) => {
   const dispatch = useDispatch();
-  
+
   const tripId = props.route.params.tripId;
   const ticketId = props.route.params.ticketId;
-  //const from = props.route.params.from;
-  //const stages = props.route.params.stages;
-  //const transportTransfers = props.route.params.transportTransfers;
-  
   const [QR, setQR] = useState('');
-/** 
-  const updateQRhandler = async () => {
-    const qr = QR;
-    await dispatch(transportActions.createTransport(tripId, qr));
-    props.navigation.navigate('Transport'),{
-        tripId: tripId,
-    };
-  };
-*/
+
   const qrHandler = async (e) => {
     setQR(e.data);
     const qr = e.data;
     await dispatch(transportActions.updateTransport(tripId, ticketId, qr));
-    props.navigation.navigate('Transport'),{
+    props.navigation.navigate('Transport'),
+      {
         tripId: tripId,
-    };
+      };
   };
 
   return (
     <QRCodeScanner
       onRead={qrHandler}
+      /**TO ADD FLASHLIGHT SWITCH BUTTON */
       //flashMode={RNCamera.Constants.FlashMode.torch}
       topContent={
         <Text style={styles.centerText}>
@@ -65,16 +47,15 @@ const AddQR = (props) => {
           {QR ? <QRCode value={QR} /> : null}
         </Text>
       }
-      /**
+      
         bottomContent={
         <TouchableOpacity style={styles.buttonTouchable}>
-          <Text style={styles.buttonText}>OK. Got it!</Text>
+          <Text style={styles.buttonText}>Track Ticket's QR-code</Text>
         </TouchableOpacity>
         }
-      */
+      
     />
   );
 };
 
 export default AddQR;
-
