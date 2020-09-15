20622a4e63f54cce56b47850429afde1161676d5
CalderBriar <cethan@interia.pl>
2020-09-15 21:05:48 +0200

    add Startup Screen

---
+++ src/domains/user/containers/StartupScreen.js
@@ -0,0 +1,49 @@
+import React, {useEffect} from 'react';
+
+import {View, ActivityIndicator, AsyncStorage} from 'react-native';
+
+import {useDispatch} from 'react-redux';
+import {StartupStyle as styles} from './AuthStyle';
+import Colors from 'constants/Colors';
+
+import * as authActions from 'user/state/Actions';
+
+const StartupScreen = (props) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const tryLogin = async () => {
+      const userData = await AsyncStorage.getItem('userData');
+      if (!userData) {
+        props.navigation.navigate('Auth');
+        return;
+      }
+      const transformedData = JSON.parse(userData);
+      const {token, userId, expiryDate} = transformedData;
+      const expirationDate = new Date(expiryDate);
+
+      if (expirationDate <= new Date() || !token || !userId) {
+        props.navigation.navigate('Auth');
+        return;
+      }
+
+      //props.navigation.navigate('Trip')
+      dispatch(authActions.authenticate(userId, token));
+    };
+
+    tryLogin();
+  }, [dispatch]);
+
+  return (
+    <View style={styles.screen}>
+      <ActivityIndicator size="large" color={Colors.white} />
+    </View>
+  );
+};
+
+export const StartupScreenOptions = {
+  headerShown: false,
+  //headerTitle: 'Authenticate',
+};
+
+export default StartupScreen;
