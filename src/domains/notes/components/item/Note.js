8ecbd4727dbc10395865822921ea31dcac801eb1
Maciej Kowalczyk <mackow3@st.amu.edu.pl>
2020-09-21 20:02:01 +0200

    List-ish items display in To Do category

--- src/domains/notes/components/item/Note.js
+++ src/domains/notes/components/item/Note.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import {ScrollView, View, Text, TouchableOpacity, Platform} from 'react-native';
+import { ScrollView, View, Text, TouchableOpacity, Platform, FlatList } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 /* imports from within the module */
 import Card from 'components/card/Card';
 import ReadMore from 'components/readMore/ReadMore';
-import {noteStyle as styles} from './NoteStyle';
+import { noteStyle as styles } from './NoteStyle';
 
 /* note item component */
 const Note = (props) => {
+
+  var ToDoList = [];
+  ToDoList = props.description.split(" ").join('\n')
+  category = props.category;
   var convertedId = props.id.split(' ');
   var extractedDate =
     convertedId[2] +
@@ -22,39 +26,59 @@ const Note = (props) => {
   /* const EditHandler = useCallback(() => { }, [dispatch, props.tripId, props.id]); */
 
   return (
-    <Card style={styles.noteCard}>
-      {/* ACTIONS BAR */}
-      <View style={styles.actions}>
-        <Text numberOfLines={1} style={styles.subtitle}>
-          {props.category}
-        </Text>
-
-        {/* EDIT BUTTON
-         <TouchableOpacity>
-          <Icon
-            name={Platform.OS === 'android' ? 'md-brush' : 'ios-brush'}
-            style={styles.icon}
-          />
-         </TouchableOpacity> */}
-
-        {/* DELETE BUTTON */}
-        <TouchableOpacity onPress={props.deleteNoteHandler}>
-          <Icon
-            name={Platform.OS === 'android' ? 'md-trash' : 'ios-trash'}
-            style={styles.icon}
-          />
-        </TouchableOpacity>
-      </View>
-
-      {/* NOTE CONTENT */}
-      <ScrollView style={styles.bodyMargin}>
-        <Text style={styles.create}>{extractedDate}</Text>
-        <Text style={styles.category}>{props.title}</Text>
-        <View style={[styles.alignText]}>
-          <ReadMore longText={props.description} />
+    <ScrollView>
+      { category === 'To Pack' ? (
+         <Card style={styles.noteCard}>
+         {/* ACTIONS BAR */}
+         <View style={styles.actions}>
+           <Text numberOfLines={1} style={styles.subtitle}>
+             {props.category}
+           </Text>
+           <TouchableOpacity onPress={props.deleteNoteHandler}>
+            <Icon
+              name={Platform.OS === 'android' ? 'md-trash' : 'ios-trash'}
+              style={styles.icon}
+            />
+          </TouchableOpacity>
         </View>
-      </ScrollView>
-    </Card>
+
+        {/* NOTE CONTENT */}
+        <ScrollView style={styles.bodyMargin}>
+          <Text style={styles.create}>{extractedDate}</Text>
+          <View style={[styles.alignText]}>
+            <ReadMore longText={ToDoList} />
+          </View>
+        </ScrollView>
+      </Card>
+
+      ) : ( 
+        <Card style={styles.noteCard}>
+         {/* ACTIONS BAR */}
+         <View style={styles.actions}>
+           <Text numberOfLines={1} style={styles.subtitle}>
+             {props.category}{console.log(ToDoList)}
+           </Text>
+           <TouchableOpacity onPress={props.deleteNoteHandler}>
+            <Icon
+              name={Platform.OS === 'android' ? 'md-trash' : 'ios-trash'}
+              style={styles.icon}
+            />
+          </TouchableOpacity>
+        </View>
+
+        {/* NOTE CONTENT */}
+        <ScrollView style={styles.bodyMargin}>
+          <Text style={styles.create}>{extractedDate}</Text>
+          <Text style={styles.category}>{props.title}</Text>
+          <View style={[styles.alignText]}>
+            <ReadMore longText={props.description} />
+          </View>
+        </ScrollView>
+      </Card>
+
+      )}
+      
+    </ScrollView>
   );
 };
 
