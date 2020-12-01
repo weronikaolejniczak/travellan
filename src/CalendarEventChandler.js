817d5d30629befe27ff5784f0ec914deb817e27f
Maciej Kowalczyk <mackow3@st.amu.edu.pl>
2020-12-01 16:42:45 +0100

    Package installation and class for adding event to calendar

---
+++ src/CalendarEventChandler.js
@@ -0,0 +1,87 @@
+//Import library for AddCalendarEvent
+import
+  * as AddCalendarEvent
+from 'react-native-add-calendar-event';
+
+//Import moment.js to deal with time
+import moment from 'moment';
+
+
+class AddEventToCalendar {
+
+    addToCalendar = (title, startDateUTC) => {
+        const eventConfig = {
+          title,
+          startDate: utcDateToString(startDateUTC),
+          endDate: 
+          utcDateToString(moment.utc(startDateUTC).add(1, 'hours')),
+          notes: 'tasty!',
+          navigationBarIOS: {
+            tintColor: 'orange',
+            backgroundColor: 'green',
+            titleColor: 'blue',
+          },
+        };
+    
+        AddCalendarEvent.presentEventCreatingDialog(eventConfig)
+        .then((eventInfo) => {
+          alert('eventInfo -> ' + JSON.stringify(eventInfo));
+        })
+        .catch((error) => {
+          // handle error such as when user rejected permissions
+          alert('Error -> ' + error);
+        });
+    };
+
+
+    
+    editCalendarEventWithId = (eventId) => {
+    if (!eventId) {
+      alert('Please Insert Event Id');
+      return;
+    }
+    const eventConfig = {
+      eventId,
+    };
+  
+    AddCalendarEvent.presentEventEditingDialog(eventConfig)
+      .then((eventInfo) => {
+        alert('eventInfo -> ' + JSON.stringify(eventInfo));
+      })
+      .catch((error) => {
+        alert('Error -> ' + error);
+      });
+  };
+
+  showCalendarEventWithId = (eventId) => {
+    if (!eventId) {
+      alert('Please Insert Event Id');
+      return;
+    }
+    const eventConfig = {
+      eventId,
+      allowsEditing: true,
+      allowsCalendarPreview: true,
+      navigationBarIOS: {
+        tintColor: 'orange',
+        backgroundColor: 'green',
+      },
+    };
+  
+    AddCalendarEvent.presentEventViewingDialog(eventConfig)
+      .then((eventInfo) => {
+        alert('eventInfo -> ' + JSON.stringify(eventInfo));
+      })
+      .catch((error) => {
+        alert('Error -> ' + error);
+      });
+  };
+
+    
+
+}
+
+export const AddEventToCalendar = new AddEventToCalendar()
+
+
+
