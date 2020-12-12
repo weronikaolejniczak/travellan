2953fe8a508feee694cd545a7132c7cd4ddb0221
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-12-12 22:18:16 +0100

    (355) Refactor fetch to axios in user actions

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {MAIN_FIREBASE_API} from 'react-native-dotenv';
 
@@ -9,73 +10,70 @@ export const authenticate = (userId, token) => {
   return {type: AUTHENTICATE, userId: userId, token: token};
 };
 
-export const signup = (email, password) => {
+export const signUpRequest = (email, password) => {
   return async (dispatch) => {
-    const response = await fetch(
+    const response = await axios.post(
       `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
       {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email: email,
-          password: password,
-          returnSecureToken: true,
-        }),
+        email: email,
+        password: password,
+        returnSecureToken: true,
       },
     );
+
     if (!response.ok) {
       const errorResData = await response.json();
       const errorId = errorResData.error.message;
       let message = 'Something went wrong!';
+
       if (errorId === 'EMAIL_EXISTS') {
-        message = 'This email exists already!';
+        message = "Email isn't correct!";
       }
+
       throw new Error(message);
     }
-    const resData = await response.json();
-    dispatch(authenticate(resData.localId, resData.idToken));
+    const data = await response.json();
+    dispatch(authenticate(data.localId, data.idToken));
+
     const expirationDate = new Date(
-      new Date().getTime() + parseInt(resData.expiresIn, 10) * 1000,
+      new Date().getTime() + parseInt(data.expiresIn, 10) * 1000,
     );
-    saveDataToStorage(resData.idToken, resData.localId, expirationDate);
+    saveDataToStorage(data.idToken, data.localId, expirationDate);
   };
 };
 
-export const login = (email, password) => {
+export const loginRequest = (email, password) => {
   return async (dispatch) => {
-    const response = await fetch(
+    const response = await axios.post(
       `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`,
       {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email: email,
-          password: password,
-          returnSecureToken: true,
-        }),
+        email: email,
+        password: password,
+        returnSecureToken: true,
       },
     );
+
     if (!response.ok) {
       const errorResData = await response.json();
       const errorId = errorResData.error.message;
       let message = 'Something went wrong!';
+
       if (errorId === 'EMAIL_NOT_FOUND') {
         message = 'Something went wrong. Try Again';
       } else if (errorId === 'INVALID_PASSWORD') {
         message = 'Something went wrong. Try Again';
       }
+
       throw new Error(message);
     }
-    const resData = await response.json();
-    dispatch(authenticate(resData.localId, resData.idToken));
+
+    const data = await response.json();
+    dispatch(authenticate(data.localId, data.idToken));
+
     const expirationDate = new Date(
-      new Date().getTime() + parseInt(resData.expiresIn, 10) * 1000,
+      new Date().getTime() + parseInt(data.expiresIn, 10) * 1000,
     );
-    saveDataToStorage(resData.idToken, resData.localId, expirationDate);
+    saveDataToStorage(data.idToken, data.localId, expirationDate);
   };
 };
 
