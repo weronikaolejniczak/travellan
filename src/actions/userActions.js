7c58201f186e97381b3ff03ab4c4ae579b0bae19
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-12-12 22:33:14 +0100

    (355) Refactor user actions to use .then notation

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -11,69 +11,54 @@ export const authenticate = (userId, token) => {
 };
 
 export const signUpRequest = (email, password) => {
-  return async (dispatch) => {
-    const response = await axios.post(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
-      {
-        email: email,
-        password: password,
-        returnSecureToken: true,
-      },
-    );
-
-    if (!response.ok) {
-      const errorResData = await response.json();
-      const errorId = errorResData.error.message;
-      let message = 'Something went wrong!';
-
-      if (errorId === 'EMAIL_EXISTS') {
-        message = "Email isn't correct!";
-      }
-
-      throw new Error(message);
-    }
-    const data = await response.json();
-    dispatch(authenticate(data.localId, data.idToken));
-
-    const expirationDate = new Date(
-      new Date().getTime() + parseInt(data.expiresIn, 10) * 1000,
-    );
-    saveDataToStorage(data.idToken, data.localId, expirationDate);
+  return async function (dispatch) {
+    await axios
+      .post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        },
+      )
+      .then((res) => res.json())
+      .then((data) => {
+        const expirationDate = new Date(
+          new Date().getTime() + parseInt(data.expiresIn, 10) * 1000,
+        );
+        dispatch(authenticate(data.localId, data.idToken));
+        saveDataToStorage(data.idToken, data.localId, expirationDate);
+      })
+      .catch(() => {
+        let message = 'Something went wrong!';
+        throw new Error(message);
+      });
   };
 };
 
 export const loginRequest = (email, password) => {
-  return async (dispatch) => {
-    const response = await axios.post(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`,
-      {
-        email: email,
-        password: password,
-        returnSecureToken: true,
-      },
-    );
-
-    if (!response.ok) {
-      const errorResData = await response.json();
-      const errorId = errorResData.error.message;
-      let message = 'Something went wrong!';
-
-      if (errorId === 'EMAIL_NOT_FOUND') {
-        message = 'Something went wrong. Try Again';
-      } else if (errorId === 'INVALID_PASSWORD') {
-        message = 'Something went wrong. Try Again';
-      }
-
-      throw new Error(message);
-    }
-
-    const data = await response.json();
-    dispatch(authenticate(data.localId, data.idToken));
-
-    const expirationDate = new Date(
-      new Date().getTime() + parseInt(data.expiresIn, 10) * 1000,
-    );
-    saveDataToStorage(data.idToken, data.localId, expirationDate);
+  return async function (dispatch) {
+    await axios
+      .post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`,
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        },
+      )
+      .then((res) => res.json())
+      .then((data) => {
+        const expirationDate = new Date(
+          new Date().getTime() + parseInt(data.expiresIn, 10) * 1000,
+        );
+        dispatch(authenticate(data.localId, data.idToken));
+        saveDataToStorage(data.idToken, data.localId, expirationDate);
+      })
+      .catch(() => {
+        const message = 'Email or password are incorrect. Please try again.';
+        throw new Error(message);
+      });
   };
 };
 
