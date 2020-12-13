8507765414ddffa29bcfcb77dc397830e9ae1f4c
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-12-13 14:38:08 +0100

    (357) Delete enableES5() from store file

--- src/store.js
+++ src/store.js
@@ -1,10 +1,7 @@
-import {enableES5} from 'immer';
 import {applyMiddleware, compose, createStore} from 'redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from 'reducers/rootReducer';
-
-enableES5();
 
 const middleware = [thunk];
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
