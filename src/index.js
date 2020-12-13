ae5624a91daed8dea05dc644048dc6ae178b9972
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-12-13 14:06:11 +0100

    (357) Enable ES5 syntax for Immer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 
 import Navigation from './routes';
-import { store } from './store';
+import {store} from './store';
 
 export default function App() {
   return (
