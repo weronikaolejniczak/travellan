39caedf8f6d569158e2a3098af7aea3f033519c4
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-12-13 21:09:31 +0100

    (357) Fix small issues

--- src/domains/trips/containers/TripsContainer.js
+++ src/domains/trips/containers/TripsContainer.js
@@ -1,20 +1,8 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import {
-  Text,
-  View,
-  Alert,
-  TouchableHighlight,
-  FlatList,
-<<<<<<< HEAD:src/domains/trips/containers/TripsContainer.js
-  Platform,
-=======
-  Button,
->>>>>>> 8084622 ((344) Change icons to MaterialCommunityIcons):src/domains/trips/containers/overview/TripsOverview.js
-} from 'react-native';
-import { useSelector, useDispatch} from 'react-redux';
-import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import React, {useState, useEffect, useCallback} from 'react';
+import {Text, View, Alert, TouchableHighlight, FlatList} from 'react-native';
+import {useSelector, useDispatch} from 'react-redux';
+import {HeaderButtons, Item} from 'react-navigation-header-buttons';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-<<<<<<< HEAD:src/domains/trips/containers/TripsContainer.js
 import SplashScreen from 'react-native-splash-screen';
 
 import {ItemlessFrame, LoadingFrame} from 'components/frames';
@@ -22,16 +10,6 @@ import {TripItem} from '../components';
 import HeaderButton from 'components/headerButton/HeaderButton';
 import * as tripsActions from 'actions/tripsActions';
 import {styles} from './TripsContainerStyle';
-=======
-import SplashScreen from 'react-native-splash-screen'
-
-import Itemless from 'components/frames/itemless/Itemless';
-import Loading from 'components/frames/loading/Loading';
-import TripItem from 'trips/components/item/Trip';
-import HeaderButton from 'components/headerButton/HeaderButton';
-import * as tripActions from 'state/trip/tripActions';
-import { tripsOverviewStyle as styles } from './TripsOverviewStyle';
->>>>>>> 8084622 ((344) Change icons to MaterialCommunityIcons):src/domains/trips/containers/overview/TripsOverview.js
 import Colors from 'constants/Colors';
 
 const TripsContainer = (props) => {
@@ -48,17 +26,10 @@ const TripsContainer = (props) => {
 
   useEffect(() => {
     loadTrips();
-<<<<<<< HEAD:src/domains/trips/containers/TripsContainer.js
     SplashScreen.hide();
   }, [loadTrips]);
 
   const handleSelectItem = (id, destination) => {
-=======
-    SplashScreen.hide()
-  }, [dispatch, loadTrips]);
-
-  const selectItemHandler = (id, destination) => {
->>>>>>> 8084622 ((344) Change icons to MaterialCommunityIcons):src/domains/trips/containers/overview/TripsOverview.js
     props.navigation.navigate('Details', {
       tripId: id,
       tripDestination: destination,
@@ -104,13 +75,8 @@ const TripsContainer = (props) => {
     );
   }
 
-<<<<<<< HEAD:src/domains/trips/containers/TripsContainer.js
   if (Array.isArray(trips) && trips.length < 1) {
     return <ItemlessFrame message={'You have no trips saved!'} />;
-=======
-  if (trips.length === 0 || trips === undefined) {
-    return <Itemless message="You have no trips saved!" />;
->>>>>>> 8084622 ((344) Change icons to MaterialCommunityIcons):src/domains/trips/containers/overview/TripsOverview.js
   }
 
   return (
@@ -139,13 +105,7 @@ const TripsContainer = (props) => {
   );
 };
 
-<<<<<<< HEAD:src/domains/trips/containers/TripsContainer.js
-/** we export screenOptions to use in our Stack.Navigator
- * @param {*} navData: lets us use "navigation" prop from within this function */
 export const tripsOptions = (navData) => {
-=======
-export const tripsOverviewOptions = (navData) => {
->>>>>>> 8084622 ((344) Change icons to MaterialCommunityIcons):src/domains/trips/containers/overview/TripsOverview.js
   return {
     headerLeft: null,
     headerRight: () => (
@@ -153,15 +113,8 @@ export const tripsOverviewOptions = (navData) => {
         <Item
           title="Add trip"
           style={{marginRight: 3}}
-<<<<<<< HEAD:src/domains/trips/containers/TripsContainer.js
-          iconName={Platform.OS === 'android' ? 'md-add' : 'ios-add'}
-          onPress={() => {
-            navData.navigation.navigate('Add trip');
-          }}
-=======
           iconName="plus"
-          onPress={() => navData.navigation.navigate('New trip')}
->>>>>>> 8084622 ((344) Change icons to MaterialCommunityIcons):src/domains/trips/containers/overview/TripsOverview.js
+          onPress={() => navData.navigation.navigate('Add trip')}
         />
       </HeaderButtons>
     ),
