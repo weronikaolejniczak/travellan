64c526fcac7fe0a6a99b7c5519657025234f2b7c
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-12-13 13:38:34 +0100

    (357) Use produce() from immer in commonReducer

--- src/reducers/commonReducer.js
+++ src/reducers/commonReducer.js
@@ -1,3 +1,5 @@
+import produce from 'immer';
+
 import {SET_TRIPS, DELETE_TRIP, CREATE_TRIP} from 'actions/tripsActions';
 import {SET_TRANSPORT, SET_QR, SET_PDF} from 'actions/transportActions';
 import {SET_ACCOMMODATION} from 'actions/accommodationActions';
@@ -10,64 +12,67 @@ export const initialState = {
 };
 
 export default (state = initialState, action) => {
-  const tripId = action.tripId;
-  const tripIndex = state.trips.findIndex((trip) => trip.id === tripId);
-  const updatedTrips = [...state.trips];
+  return produce(state, (draft) => {
+    const tripId = action.tripId;
+    const tripIndex = draft.trips.findIndex((trip) => trip.id === tripId);
+    const updatedTrips = [...draft.trips];
 
-  switch (action.type) {
-    case SET_TRIPS:
-      return {
-        trips: action.trips,
-      };
-    case DELETE_TRIP:
-      return {
-        ...state,
-        trips: state.trips.filter((item) => item.id !== tripId),
-      };
-    case CREATE_TRIP:
-      return {
-        ...state,
-        trips: state.trips.concat(action.newTrip),
-      };
+    switch (action.type) {
+      case SET_TRIPS:
+        return {
+          trips: action.trips,
+        };
+      case DELETE_TRIP:
+        return {
+          ...draft,
+          trips: draft.trips.filter((item) => item.id !== tripId),
+        };
+      case CREATE_TRIP:
+        return {
+          ...draft,
+          trips: draft.trips.concat(action.newTrip),
+        };
 
-    case SET_TRANSPORT:
-    case SET_QR:
-    case SET_PDF:
-      updatedTrips[tripIndex].transport = action.transport;
+      case SET_TRANSPORT:
+      case SET_QR:
+      case SET_PDF:
+        updatedTrips[tripIndex].transport = action.transport;
 
-      return {
-        ...state,
-        trips: updatedTrips,
-      };
+        return {
+          ...draft,
+          trips: updatedTrips,
+        };
 
-    case SET_ACCOMMODATION:
-      updatedTrips[tripIndex].accommodation = action.accommodation;
+      case SET_ACCOMMODATION:
+        updatedTrips[tripIndex].accommodation = action.accommodation;
 
-      return {
-        ...state,
-        trips: updatedTrips,
-      };
+        return {
+          ...draft,
+          trips: updatedTrips,
+        };
 
-    case SET_NOTES:
-      updatedTrips[tripIndex].notes = action.notes;
-      return {
-        ...state,
-        trips: updatedTrips,
-      };
+      case SET_NOTES:
+        updatedTrips[tripIndex].notes = action.notes;
+        return {
+          ...draft,
+          trips: updatedTrips,
+        };
 
-    case SET_BUDGET:
-      updatedTrips[tripIndex].budget = action.budget;
-      return {
-        ...state,
-        trips: updatedTrips,
-      };
+      case SET_BUDGET:
+        updatedTrips[tripIndex].budget = action.budget;
+        return {
+          ...draft,
+          trips: updatedTrips,
+        };
 
-    case SET_MAP:
-      updatedTrips[tripIndex].map = action.map;
-      return {
-        ...state,
-        trips: updatedTrips,
-      };
-  }
-  return state;
+      case SET_MAP:
+        updatedTrips[tripIndex].map = action.map;
+        return {
+          ...draft,
+          trips: updatedTrips,
+        };
+    }
+
+    return draft;
+  });
 };
