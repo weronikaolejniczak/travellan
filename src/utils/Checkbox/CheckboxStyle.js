d707d0ed3325122a4fbe8973b7fbdab1129c3a9c
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-12-20 21:41:47 +0100

    (399) create Checkbox component (no need for CheckboxGroup)

---
+++ src/utils/Checkbox/CheckboxStyle.js
@@ -0,0 +1,8 @@
+import Colors from 'constants/Colors';
+
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: { alignItems: 'center', display: 'flex', flexDirection: 'row' },
+  label: { color: Colors.text, fontSize: 17 },
+});
