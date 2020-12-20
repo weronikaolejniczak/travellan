292831fb74dd125d2ca6ab1a2b510cedffa97b38
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-12-20 15:32:18 +0100

    (399) export utils

---
+++ src/utils/index.js
@@ -0,0 +1,6 @@
+export { default as Card } from './Card/Card';
+export { default as HeaderButton } from './HeaderButton';
+export { default as ItemlessFrame } from './Frames/ItemlessFrame/ItemlessFrame';
+export { default as LoadingFrame } from './Frames/LoadingFrame/LoadingFrame';
+export { default as ReadMore } from './ReadMore';
+export { default as Switch } from './Switch';
