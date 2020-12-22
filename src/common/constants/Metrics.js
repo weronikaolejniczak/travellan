cf47bc0955bbb76b8b5b1446ec69a9737c153192
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-12-22 18:19:01 +0100

    (399) refactor Metrics, use margin in Button component

--- src/common/constants/Metrics.js
+++ src/common/constants/Metrics.js
@@ -1,75 +1,85 @@
-/**
- * This file contains metric values that are global to the application
- */
-import { Dimensions } from 'react-native';
-
-// screen's dimensions
-const { height, width } = Dimensions.get('window');
-
-// responsive metrics
-export const tiny = 5;
-export const small = tiny * 2; // 10
-export const normal = tiny * 3; // 15
-export const medium = normal * 2; // 30
+export const tiny = 2;
+export const small = tiny * 2; // 4
+export const normal = tiny * 3; // 6
+export const medium = normal * 2; // 12
 
 export default {
-  // bottom margin
   bottomMargin: {
     marginBottom: normal,
-  },
-  mediumBottomMargin: {
-    marginBottom: medium,
-  },
-  // vertical margin
-  tinyVerticalMargin: {
-    marginVertical: tiny,
-  },
-  smallVerticalMargin: {
-    marginVertical: small,
-  },
-  verticalMargin: {
-    marginVertical: normal,
-  },
-  mediumVerticalMargin: {
-    marginVertical: medium,
-  },
-  // horizontal margin
-  tinyHorizontalMargin: {
-    marginHorizontal: tiny,
-  },
-  smallHorizontalMargin: {
-    marginHorizontal: small,
   },
   horizontalMargin: {
     marginHorizontal: normal,
   },
-  mediumHorizontalMargin: {
-    marginHorizontal: medium,
-  },
-  // horizontal padding
-  tinyHorizontalPadding: {
-    paddingHorizontal: tiny,
-  },
-  smallHorizontalPadding: {
-    paddingHorizontal: small,
-  },
   horizontalPadding: {
     paddingHorizontal: normal,
+  },
+  margin: {
+    margin: normal,
+  },
+  mediumBottomMargin: {
+    marginBottom: medium,
+  },
+  mediumHorizontalMargin: {
+    marginHorizontal: medium,
   },
   mediumHorizontalPadding: {
     paddingHorizontal: medium,
   },
-  // vertical padding
-  tinyVerticalPadding: {
-    paddingVertical: tiny,
+  mediumMargin: {
+    margin: medium,
+  },
+  mediumPadding: {
+    padding: medium,
+  },
+  mediumVerticalMargin: {
+    marginVertical: medium,
+  },
+  mediumVerticalPadding: {
+    paddingVertical: medium,
+  },
+  padding: {
+    padding: normal,
+  },
+  smallHorizontalMargin: {
+    marginHorizontal: small,
+  },
+  smallHorizontalPadding: {
+    paddingHorizontal: small,
+  },
+  smallMargin: {
+    margin: small,
+  },
+  smallPadding: {
+    padding: small,
+  },
+  smallVerticalMargin: {
+    marginVertical: small,
   },
   smallVerticalPadding: {
     paddingVertical: small,
   },
+  tinyHorizontalMargin: {
+    marginHorizontal: tiny,
+  },
+  tinyHorizontalPadding: {
+    paddingHorizontal: tiny,
+  },
+  tinyMargin: {
+    margin: tiny,
+  },
+  tinyPadding: {
+    padding: tiny,
+  },
+  tinyVerticalMargin: {
+    marginVertical: tiny,
+  },
+  tinyVerticalPadding: {
+    paddingVertical: tiny,
+  },
+  verticalMargin: {
+    marginVertical: normal,
+  },
   verticalPadding: {
     paddingVertical: normal,
-  },
-  mediumVerticalPadding: {
-    paddingVertical: medium,
   },
 };
