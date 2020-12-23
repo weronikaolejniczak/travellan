58230edeaa3a8585d2386af32b1d029deb0d7952
Shanhavael <konrad.skrzypczak1@op.pl>
2020-12-24 00:33:22 +0100

    merge

--- src/domains/trips/components/index.js
+++ src/domains/trips/components/index.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-export {default as NavigationButton} from './navigationButton/NavigationButton';
-export {default as TripItem} from './TripItem/TripItem';
-=======
-export { default as NavigationButton } from '../components/navigationButton/NavigationButton';
+export { default as NavigationButton } from './navigationButton/NavigationButton';
 export { default as TripItem } from './TripItem/TripItem';
->>>>>>> develop
