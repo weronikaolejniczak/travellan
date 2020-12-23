ab0e83db05a33dbcabaf8c6a461f7cbab57c445f
CalderBriar <cethan@interia.pl>
2020-12-23 21:06:40 +0100

    Resolve additional conflicts

--- src/domains/trips/components/index.js
+++ src/domains/trips/components/index.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-export { default as NavigationButton } from 'src/domains/trips/components/navigationButton/NavigationButton.js';
-export { default as TripItem } from 'src/domains/trips/components/TripItem/TripItem.js';
-=======
 export { default as NavigationButton } from '../components/navigationButton/NavigationButton';
 export { default as TripItem } from './TripItem/TripItem';
->>>>>>> c96062ec328da4913cd3cf4135194bf5d0c5eb1e
