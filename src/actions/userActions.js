f2c7be6bcc39a58a3c4f6de2da0f7899de86988f
CalderBriar <cethan@interia.pl>
2021-01-05 23:59:44 +0100

    (425) Add dependency usage

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { MAIN_FIREBASE_API } from 'react-native-dotenv';
+import auth from '@react-native-firebase/auth';
 
 export const AUTHENTICATE = 'AUTHENTICATE';
 
