5f502a2b41cb86eccc421029603f2067b188d84d
Maciej Kowalczyk <mackow3@st.amu.edu.pl>
2021-01-05 20:12:50 +0100

    Deletion of unused styles from NotificationContainerStyle according to review from github

--- src/domains/user/containers/NotificationContainerStyle.js
+++ src/domains/user/containers/NotificationContainerStyle.js
@@ -8,43 +8,6 @@ export const styles = StyleSheet.create({
     paddingTop: '12%',
     paddingHorizontal: '5%',
   },
-  label: {
-    color: Colors.primary,
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  input: {
-    color: Colors.text,
-    fontSize: 18,
-    borderBottomColor: Colors.primary,
-    borderBottomWidth: 1,
-  },
-  buttonContainer: {
-    marginTop: '10%',
-    alignItems: 'center',
-  },
-  button: {
-    borderRadius: 25,
-    backgroundColor: Colors.primary,
-    alignItems: 'center',
-    width: '40%',
-    padding: 15,
-  },
-  buttonText: {
-    fontWeight: 'bold',
-    fontSize: 16,
-    color: Colors.text,
-  },
-  smallPaddingTop: {
-    paddingTop: '5%',
-  },
-  errorContainer: {
-    marginVertical: 5,
-    marginHorizontal: 40,
-  },
-  error: {
-    color: Colors.error,
-  },
   itemlessContainer: {
     flex: 1,
     justifyContent: 'center',
