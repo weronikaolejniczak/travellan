a47e59f597acdf71944e5feb5b71b251d064cb4a
CalderBriar <cethan@interia.pl>
2021-01-09 00:01:18 +0100

    (425) Fix error colors

--- src/domains/user/containers/RegisterContainer.js
+++ src/domains/user/containers/RegisterContainer.js
@@ -167,7 +167,7 @@ const RegisterContainer = (props) => {
                 />
                 {touched.email && errors.email && (
                   <View style={styles.errorContainer}>
-                    <Text style={{ color: Colors.red }}>{errors.email}</Text>
+                    <Text style={{ color: Colors.error }}>{errors.email}</Text>
                   </View>
                 )}
               </View>
@@ -182,7 +182,7 @@ const RegisterContainer = (props) => {
                 />
                 {touched.password && errors.password && (
                   <View style={styles.errorContainer}>
-                    <Text style={{ color: Colors.red }}>
+                    <Text style={{ color: Colors.error }}>
                       {errors.password}
                     </Text>
                   </View>
@@ -199,7 +199,7 @@ const RegisterContainer = (props) => {
                 />
                 {touched.confirmPassword && errors.confirmPassword && (
                   <View style={styles.errorContainer}>
-                    <Text style={{ color: Colors.red }}>
+                    <Text style={{ color: Colors.error }}>
                       {errors.confirmPassword}
                     </Text>
                   </View>
