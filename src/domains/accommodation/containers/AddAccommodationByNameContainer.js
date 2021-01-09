abc0aad6bf1f6b3c58a8faf6d2f71efb9a3a9db0
mkowalczyk97 <mackow3@st.amu.edu.pl>
2021-01-09 17:50:11 +0100

    TRIPP-304 Clean up code + placeholder namchange

--- src/domains/accommodation/containers/AddAccommodationByNameContainer.js
+++ src/domains/accommodation/containers/AddAccommodationByNameContainer.js
@@ -1,64 +1,28 @@
 import React, { useState } from 'react';
-import {
-  Button,
-  Caption,
-  Checkbox,
-  ScrollView as Container,
-  Headline,
-  Paragraph,
-  RadioButtonGroup,
-  Searchbar,
-  Subheading,
-  Switch,
-  Text,
-  TextInput,
-  Title,
-} from 'utils';
+import { Button, ScrollView as Container, Headline, TextInput } from 'utils';
 import { View } from 'react-native';
 
 const AddAccommodationByNameContainer = (props) => {
   const [enabled, setEnabled] = useState(false);
   const [error, setError] = useState('');
   const [value, setValue] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const [radioValue, setRadioValue] = useState(null);
-  const [searchQuery, setSearchQuery] = useState('');
-
-  const onChangeSearch = (query) => setSearchQuery(query);
 
   const handleChange = (val) => {
     setValue(val);
-    !value.match('@') ? setError('Invalid e-mail format') : setError('');
   };
 
   const handlePress = () => console.log('hello!');
 
-  const toggleSwitch = () => setEnabled((prevState) => !prevState);
-
-  const options = [
-    {
-      key: 'samsung',
-      text: 'Samsung123',
-    },
-    {
-      key: 'apple',
-      text: 'Apple',
-    },
-    {
-      key: 'motorola',
-      text: 'Motorola',
-    },
-    {
-      key: 'lenovo',
-      text: 'Lenovo',
-    },
-  ];
-
   return (
     <Container>
       <Headline>Headline!</Headline>
-      <TextInput error={error} onChange={handleChange} value={value} />
-      <Button onPress={handlePress}>Hello, World!</Button>
+      <TextInput
+        label={'Hotel address'}
+        error={error}
+        onChange={handleChange}
+        value={value}
+      />
+      <Button onPress={handlePress}>Submit</Button>
     </Container>
   );
 };
