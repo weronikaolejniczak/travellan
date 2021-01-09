87c39bff22d78f85d9ecee9626c81ae7247bf790
mkowalczyk97 <mackow3@st.amu.edu.pl>
2021-01-09 23:15:14 +0100

    TRIPP-307 Configuration from fetchWeather

--- src/services/fetchCityCode.js
+++ src/services/fetchCityCode.js
@@ -1,2 +1,13 @@
 import axios from 'axios';
 import { BACKEND_API_KEY, BACKEND_URL } from 'react-native-dotenv';
+
+async function fetchCityCode(latitude, longitude) {
+  return await axios
+    .get(`${BACKEND_URL}/weather?latitude=${latitude}&longitude=${longitude}`)
+    .then((json) => json.data)
+    .catch((error) => {
+      throw error;
+    });
+}
+
+export default fetchCityCode;
