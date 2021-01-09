b430a73378c99bce28f90d017a7f7decd8ce8f3a
mkowalczyk97 <mackow3@st.amu.edu.pl>
2021-01-09 21:15:02 +0100

    TRIPP-304 Deletion of unused styles

--- src/domains/accommodation/containers/AddAccommodationByNameStyleContainer.js
+++ src/domains/accommodation/containers/AddAccommodationByNameStyleContainer.js
@@ -2,14 +2,4 @@ import { StyleSheet } from 'react-native';
 
 import { Colors } from 'constants';
 
-export const styles = StyleSheet.create({
-  itemlessContainer: {
-    alignItems: 'center',
-    flex: 1,
-    justifyContent: 'center',
-  },
-  itemlessText: {
-    fontSize: 18,
-    textAlign: 'center',
-  },
-});
+export const styles = StyleSheet.create({});
