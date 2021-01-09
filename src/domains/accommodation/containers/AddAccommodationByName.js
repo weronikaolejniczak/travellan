4ff24cbce91afc2be44ee4b21d9a42d905eda5b0
mkowalczyk97 <mackow3@st.amu.edu.pl>
2021-01-09 16:57:10 +0100

    TRIPP-304 Rename of export

--- src/domains/accommodation/containers/AddAccommodationByName.js
+++ src/domains/accommodation/containers/AddAccommodationByName.js
@@ -16,7 +16,7 @@ import {
 } from 'utils';
 import { View } from 'react-native';
 
-const AddAccommodationContainer = (props) => {
+const AddAccommodationByName = (props) => {
   const [enabled, setEnabled] = useState(false);
   const [error, setError] = useState('');
   const [value, setValue] = useState('');
@@ -109,4 +109,4 @@ const AddAccommodationContainer = (props) => {
   );
 };
 
-export default AddAccommodationContainer;
+export default AddAccommodationByName;
