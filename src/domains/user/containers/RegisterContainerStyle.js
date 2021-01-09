79f5c58aac5f3817cf6c6625b1d6bb91b3278b59
CalderBriar <cethan@interia.pl>
2021-01-09 01:05:59 +0100

    (425) Refactor Registration Styling Sheet

--- src/domains/user/containers/RegisterContainerStyle.js
+++ src/domains/user/containers/RegisterContainerStyle.js
@@ -1,65 +1,61 @@
-import { StyleSheet } from 'react-native';
 import Colors from 'constants/Colors';
+import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
+  actionsContainer: {
     alignItems: 'center',
-    backgroundColor: Colors.background,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    marginTop: 30,
   },
   authContainer: {
     width: '80%',
   },
-  actionsContainer: {
-    marginTop: 30,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   buttonContainer: {
+    alignItems: 'center',
+    backgroundColor: Colors.primary,
+    borderRadius: 25,
     display: 'flex',
+    justifyContent: 'center',
     margin: 10,
     padding: 12,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 25,
-    backgroundColor: Colors.primary,
     width: '40%',
   },
   buttonText: {
-    fontWeight: 'bold',
+    color: Colors.text,
     fontSize: 16,
-    color: Colors.text,
-  },
-  input: {
-    color: Colors.text,
-  },
-  
-  //break
-  formControl: {
-    width: '100%',
-  },
-  label: {
-    color: Colors.text,
     fontWeight: 'bold',
-    fontSize: 18,
-    marginVertical: 8,
-  },
-  input: {
-    color: Colors.text,
-    fontSize: 14,
-    paddingHorizontal: 2,
-    paddingVertical: 5,
-    borderBottomColor: Colors.text,
-    borderBottomWidth: 1,
   },
   errorContainer: {
     marginVertical: 5,
   },
+
   errorText: {
-    fontFamily: 'open-sans',
     color: 'red',
+    fontFamily: 'open-sans',
     fontSize: 13,
+  },
+  formControl: {
+    width: '100%',
+  },
+  input: {
+    borderBottomColor: Colors.text,
+    borderBottomWidth: 1,
+    color: Colors.text,
+    fontSize: 14,
+    paddingHorizontal: 2,
+    paddingVertical: 5,
+  },
+  label: {
+    color: Colors.text,
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginVertical: 8,
+  },
+  screen: {
+    alignItems: 'center',
+    backgroundColor: Colors.background,
+    flex: 1,
+    justifyContent: 'center',
   },
 });
