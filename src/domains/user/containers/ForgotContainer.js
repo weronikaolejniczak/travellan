3a90dc14c24400b004dccdc56462fcee19af58d1
CalderBriar <cethan@interia.pl>
2021-01-10 01:11:48 +0100

    TRIPP-383 Implement basic screen view for ForgotContainer

--- src/domains/user/containers/ForgotContainer.js
+++ src/domains/user/containers/ForgotContainer.js
@@ -1,9 +1,66 @@
+import * as yup from 'yup';
+import React, { useEffect, useState } from 'react';
+import {
+  Alert,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  View,
+} from 'react-native';
 import { View as Container } from 'utils';
+import { Formik } from 'formik';
+
+import { Button, TextInput } from 'utils';
+import { styles } from './ForgotContainerStyle';
+import { useDispatch } from 'react-redux';
 
 const ForgotContainer = (props) => {
-  return(
-  <Container></Container>
-  )
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    if (error) {
+      Alert.alert('An error occured!', error, [{ text: 'Okay' }]);
+    }
+  }, [error]);
+
+  return (
+    <Formik
+      initialValues={{
+        email: '',
+      }}
+      onSubmit={(values) => {
+        //placeholder
+      }}
+      validationSchema={yup.object().shape({
+        email: yup
+          .string()
+          .email('Invalid email address')
+          .required('Cannot be left empty'),
+      })}
+    >
+      {({ values, handleChange, errors, isValid, handleSubmit }) => (
+        <Container>
+          <TextInput
+            value={values.email}
+            style={styles.input}
+            onChange={handleChange('email')}
+            autoCapitalize="none"
+            label="E-mail"
+            error={errors.email}
+          />
+          <Button
+            loading={isLoading}
+            disabled={isLoading}
+            onPress={handleSubmit}
+          >
+            Send email
+          </Button>
+        </Container>
+      )}
+    </Formik>
+  );
 };
 
 export default ForgotContainer;
