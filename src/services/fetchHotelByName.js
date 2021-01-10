cbe2d30ac6242671eafbab00a7ad931e3aee6d5b
mkowalczyk97 <mackow3@st.amu.edu.pl>
2021-01-10 17:47:32 +0100

    TRIPP-307 proper call to api

--- src/services/fetchHotelByName.js
+++ src/services/fetchHotelByName.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 import { BACKEND_API_KEY, BACKEND_URL } from 'react-native-dotenv';
 
-async function fetchHotelByName(hotel) {
+async function fetchHotelByName(cityCode, hotel) {
   return await axios
-    .get(`${BACKEND_URL}`)
+    .get(
+      `${BACKEND_URL}/accommodation/hotelByName?cityCode=${cityCode}&radius=20&hotelName=${hotel}`,
+    )
     .then((json) => json.data)
     .catch((error) => {
       throw error;
