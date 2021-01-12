15fe3b4d16c1310fcd814ac95e58b27d3b3216c4
mkowalczyk97 <mackow3@st.amu.edu.pl>
2021-01-12 19:33:51 +0100

    TRIPP-496 View for edditing trip

--- src/domains/trips/containers/EditTripContainer.js
+++ src/domains/trips/containers/EditTripContainer.js
@@ -18,8 +18,56 @@ import { styles } from '././EditTripContainerStyle';
 const EditTripContainer = ({ route, navigation }) => {
   const tripId = route.params.tripId;
   const budget = route.params.budget;
+  const [destinationIsValid, setDestinationIsValid] = useState(false);
+  const [destinationSubmitted, setDestinationSubmitted] = useState(false);
   const [destination, setDestination] = useState(route.params.description);
-  return <Container keyboardShouldPersistTaps="always"></Container>;
+  const [startDate, setStartDate] = useState(new Date());
+  const [showStartDate, setShowStartDate] = useState(false);
+  const [endDate, setEndDate] = useState(new Date());
+  const [showEndDate, setShowEndDate] = useState(false);
+
+  const destinationChangeHandler = (text) => {
+    text.trim().length === 0 || !destinationRegex.test(text)
+      ? setDestinationIsValid(false)
+      : setDestinationIsValid(true);
+    setDestination(text);
+  };
+  const adjustEndDateToStartDate = (currentDate) =>
+    currentDate > endDate && setEndDate(currentDate);
+
+  return (
+    <Container keyboardShouldPersistTaps="always">
+      <TextInput
+        label="City and/or country"
+        value={destination}
+        onChange={destinationChangeHandler}
+        error={
+          !destinationIsValid &&
+          destinationSubmitted &&
+          'Enter a valid city and/or country!'
+        }
+      />
+
+      <DateTimePicker
+        label="Start date"
+        setShowPicker={setShowStartDate}
+        date={startDate}
+        showPicker={showStartDate}
+        minimumDate={Date.now()}
+        setDate={setStartDate}
+        adjustDate={adjustEndDateToStartDate}
+      />
+
+      <DateTimePicker
+        label="End date"
+        setShowPicker={setShowEndDate}
+        date={endDate}
+        showPicker={showEndDate}
+        minimumDate={startDate}
+        setDate={setEndDate}
+      />
+    </Container>
+  );
 };
 
 export default EditTripContainer;
