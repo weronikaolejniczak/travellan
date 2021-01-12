138c09af88360c2f89a4794eadfb30d59d3b4668
Weronika Olejniczak <wer.olejniczak@gmail.com>
2021-01-12 16:46:01 +0100

    TRIPP-257 fixes

--- src/services/fetchMapSearch.js
+++ src/services/fetchMapSearch.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
-import { MAPBOX_KEY } from 'react-native-dotenv';
+import { MAPBOX_API_KEY } from 'react-native-dotenv';
 
-const API_KEY = MAPBOX_KEY;
+const API_KEY = MAPBOX_API_KEY;
 
-async function fetchMapSearch(querry, longitude, latitude) {
+async function fetchMapSearch(query, longitude, latitude) {
   return await axios
     .get(
-      `https://api.mapbox.com/geocoding/v5/mapbox.places/${querry}.json?limit=1&proximity=${longitude},${latitude}&access_token=${API_KEY}`,
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?limit=1&proximity=${longitude},${latitude}&access_token=${API_KEY}`,
     )
     .then((json) => json.data.features[0]);
 }
