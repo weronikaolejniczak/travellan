8e41b81003b04d0fa9786f9c11de36bb3d3b06c5
Weronika Olejniczak <wer.olejniczak@gmail.com>
2021-01-16 21:03:19 +0100

    TRIPP-502 update accommodation thunks

--- src/models/Accommodation.js
+++ src/models/Accommodation.js
@@ -6,13 +6,14 @@ class Accommodation {
     checkInExtra,
     checkInHours,
     checkOutHours,
-    phone,
     creditCardPaymentPossible,
     description,
     frontDesk24H,
-    location,
     image,
+    location,
     name,
+    phone,
+    reservationDetails,
   ) {
     this.id = id;
     this.amenities = amenities;
@@ -20,13 +21,14 @@ class Accommodation {
     this.checkInExtra = checkInExtra;
     this.checkInHours = checkInHours;
     this.checkOutHours = checkOutHours;
-    this.phone = phone;
     this.creditCardPaymentPossible = creditCardPaymentPossible;
     this.description = description;
     this.frontDesk24H = frontDesk24H;
-    this.location = location;
     this.image = image;
+    this.location = location;
     this.name = name;
+    this.phone = phone;
+    this.reservationDetails = reservationDetails;
   }
 }
 
