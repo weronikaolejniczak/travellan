ab56422576ee028af50cca6edfe51f5f72dc934e
mkowalczyk97 <mackow3@st.amu.edu.pl>
2021-01-16 18:11:06 +0100

    TRIPP-522 Checking if startDate==endDate in order to display proper view

--- src/domains/accommodation/containers/HotelRecommendationContainer.js
+++ src/domains/accommodation/containers/HotelRecommendationContainer.js
@@ -10,6 +10,29 @@ import { View } from 'react-native';
 
 const HotelRecommendationContainer = (props) => {
   const { startDate, endDate, cityCode } = props.route.params;
+  const [isLoading, setIsLoading] = useState(false);
+  const [isDateSame, setIsDateSame] = useState(true);
+
+  const formatDate = (date) => {
+    //format to YYYY-MM-DD
+    let d = new Date(date),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+  };
+
+  useEffect(() => {
+    if (formatDate(startDate) === formatDate(endDate)) {
+      setIsDateSame(true);
+    } else {
+      setIsDateSame(false);
+    }
+  }, [startDate, endDate]);
 
   return (
     <Container>
