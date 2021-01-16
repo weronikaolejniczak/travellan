b7af534d6447d8c3276d28638343f8df8cf7c291
mkowalczyk97 <mackow3@st.amu.edu.pl>
2021-01-16 17:49:53 +0100

    TRIPP-517 Template for hotel recommendation screen

---
+++ src/domains/accommodation/containers/HotelRecommendationContainer.js
@@ -0,0 +1,15 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import {
+  Button,
+  ScrollView as Container,
+  Headline,
+  ItemlessFrame,
+  TextInput,
+} from 'utils';
+import { View } from 'react-native';
+
+const HotelRecommendationContaier = (props) => {
+  return <Container></Container>;
+};
+
+export default HotelRecommendationContaier;
