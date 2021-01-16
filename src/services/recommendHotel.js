643328df404ba2fccda6e4f67d4f8d41d1e61f3a
mkowalczyk97 <mackow3@st.amu.edu.pl>
2021-01-16 22:10:12 +0100

    TRIPP-519 Proper variables in recommendHotel function

--- src/services/recommendHotel.js
+++ src/services/recommendHotel.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 import { BACKEND_URL } from 'react-native-dotenv';
 
-async function recommendHotel(cityCode, hotel) {
+async function recommendHotel(
+  cityCode,
+  checkInDate,
+  checkOutDate,
+  radius,
+  roomQuantity,
+) {
   return await axios
     .get(`${BACKEND_URL}/accommodation/recommendation`)
     .then((json) => json.data)
