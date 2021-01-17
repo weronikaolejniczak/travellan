c35765701e4120544ead9f79fdfe5d914d6d7958
mkowalczyk97 <mackow3@st.amu.edu.pl>
2021-01-17 17:51:39 +0100

    TRIPP-521 Custom template views

---
+++ src/domains/accommodation/containers/RecommendedHotelDetails.js
@@ -0,0 +1,24 @@
+import React, { useCallback, useState } from 'react';
+import Snackbar from 'react-native-snackbar';
+import { useDispatch } from 'react-redux';
+
+import Budget from 'models/Budget';
+
+import { BudgetPicker } from 'components';
+import {
+  Button,
+  ScrollView as Container,
+  DateTimePicker,
+  TextInput,
+  Header,
+} from 'utils';
+
+const RecommendedHotelDetails = ({ navigation }) => {
+  return (
+    <Container>
+      <Header>fasdfasd</Header>
+    </Container>
+  );
+};
+
+export default RecommendedHotelDetails;
