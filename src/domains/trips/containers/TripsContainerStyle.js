5d7646ca83da98347af500af68d061b9bb714535
CalderBriar <cethan@interia.pl>
2021-01-24 22:33:37 +0100

    TRIPP-542 merge styles

--- src/domains/trips/containers/TripsContainerStyle.js
+++ src/domains/trips/containers/TripsContainerStyle.js
@@ -4,18 +4,13 @@ import { Colors } from 'constants';
 
 export const styles = StyleSheet.create({
   actionButton: {
-<<<<<<< HEAD
-    padding: 15,
-=======
     flex: 1,
     flexDirection: 'row-reverse',
->>>>>>> cf64bbd14409a499ac05ffcbddccb185db9fefbe
   },
   actionIcon: {
     color: Colors.primary,
     fontSize: 30,
   },
-<<<<<<< HEAD
   flex: {
     flex: 1,
   },
@@ -39,9 +34,7 @@ export const styles = StyleSheet.create({
   },
   triplessText: {
     fontSize: 18,
-=======
   iconWrapper: {
     padding: 15,
->>>>>>> cf64bbd14409a499ac05ffcbddccb185db9fefbe
   },
 });
