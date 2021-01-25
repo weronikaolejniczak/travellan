a6b0424920c182a5475f1f18c381d8a7be1bb331
mkowalczyk97 <mackow3@st.amu.edu.pl>
2021-01-25 21:04:54 +0100

    TRIPP-544 Images and view for sharing

--- src/domains/share/containers/SharingContainer.js
+++ src/domains/share/containers/SharingContainer.js
@@ -1,6 +1,14 @@
 import React, { useEffect } from 'react';
-import { ScrollView, Text, View } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
+
+import {
+  ScrollView as Container,
+  Text,
+  Headline,
+  Subheading,
+  Paragraph,
+} from 'utils';
+import { Image, View } from 'react-native';
 
 import { styles } from './SharingContainerStyle';
 
@@ -10,13 +18,93 @@ const SharingContainer = (props) => {
   }, []);
 
   return (
-    <ScrollView style={styles.container}>
-      <View style={styles.itemlessContainer}>
-        <Text style={[styles.text, styles.itemlessText, styles.marginTop]}>
-          Sharing
-        </Text>
+    <Container style={styles.container}>
+      <Subheading style={styles.subheading}>
+        What is Travellan Share?
+      </Subheading>
+      <Paragraph style={styles.paragraph}>
+        Along with the development of the Travellan application, option of
+        saving information about accommodation has been added. The option is
+        intended for users who have already booked their stay in a given hotel
+        and want to keep all the most important information about their
+        accommodation inside the application.
+      </Paragraph>
+      <Subheading style={styles.subheading}>
+        What pages can be shared by Travellan?
+      </Subheading>
+      <Paragraph style={styles.paragraph}>
+        Currently, Travellan share is only compatible with hotel offers on the
+        booking.com website.
+      </Paragraph>
+      <Subheading style={styles.subheading}>
+        How to use Travellan Share?
+      </Subheading>
+      <Paragraph style={styles.paragraph}>
+        <Paragraph style={([styles.paragraph], [styles.color])}>1.</Paragraph>{' '}
+        Go to the website booking.com and find your hotel offer which you
+        already booked {'\n'}
+        <Paragraph style={([styles.paragraph], [styles.color])}>
+          2.
+        </Paragraph>{' '}
+        Click on 'more' button and choose 'Share'{'\n'}
+      </Paragraph>
+      <View style={styles.imageView}>
+        <Image
+          style={styles.image}
+          source={require('assets/images/MainShare.jpg')}
+        />
       </View>
-    </ScrollView>
+      <Paragraph style={styles.paragraph}>
+        <Paragraph style={([styles.paragraph], [styles.color])}>3.</Paragraph>{' '}
+        Choose TravellanShare
+      </Paragraph>
+      <View style={styles.imageView}>
+        <Image
+          style={styles.imageSmall}
+          source={require('assets/images/ChooseTravellanShare.jpg')}
+        />
+      </View>
+      <Paragraph style={styles.paragraph}>
+        <Paragraph style={([styles.paragraph], [styles.color])}>4.</Paragraph>{' '}
+        Wait until the data will load
+      </Paragraph>
+      <View style={styles.imageView}>
+        <Image
+          style={styles.imageSmall}
+          source={require('assets/images/LoadingScreen.jpg')}
+        />
+      </View>
+      <Paragraph style={styles.paragraph}>
+        <Paragraph style={([styles.paragraph], [styles.color])}>5.</Paragraph>{' '}
+        Check and verify your hotel data
+      </Paragraph>
+      <View style={styles.imageView}>
+        <Image
+          style={styles.image}
+          source={require('assets/images/ScrappedData.jpg')}
+        />
+      </View>
+      <Paragraph style={styles.paragraph}>
+        <Paragraph style={([styles.paragraph], [styles.color])}>6.</Paragraph>{' '}
+        Choose trip in which you want to save hotel and click submit
+      </Paragraph>
+      <View style={styles.imageView}>
+        <Image
+          style={styles.imageMedium}
+          source={require('assets/images/SelectAndSubmit.jpg')}
+        />
+      </View>
+      <Paragraph style={styles.paragraph}>
+        <Paragraph style={([styles.paragraph], [styles.color])}>7.</Paragraph>{' '}
+        Go to accommodation and enjoy most needed information about your hotel!
+      </Paragraph>
+      <View style={styles.imageView}>
+        <Image
+          style={styles.image}
+          source={require('assets/images/HotelInAccommodationCard.jpg')}
+        />
+      </View>
+    </Container>
   );
 };
 
