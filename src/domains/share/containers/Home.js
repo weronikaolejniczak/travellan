579371ecf4c805b45eb623aa4980e4f5616a79f2
Weronika Olejniczak <wer.olejniczak@gmail.com>
2021-01-28 23:35:34 +0100

    hotfix

--- src/domains/share/containers/Home.js
+++ src/domains/share/containers/Home.js
@@ -21,6 +21,7 @@ import { homeStyle as styles } from './HomeStyle';
 const COULD_NOT_SCRAPE_HOTEL_ERROR = `Sorry, we couldn't get your hotel info!\nAre you sure you have internet connection?`;
 const INCORRECT_SHARING_DATA_ERROR = `You didn't share a Booking.com hotel page!\nUnfortunately, we don't support any other sharing data.`;
 const USER_NOT_LOGGED_IN_ERROR = `You are not logged in!\nPlease, log in and share the webpage again!`;
+const SHARING_INTENT_ERROR = `Hey! Something went wrong while sharing. Try again!`;
 const NO_TRIPS_SELECTED_ERROR = `You haven't selected any trip!`;
 const COULD_NOT_SAVE_ACCOMMODATION_ERROR = `Sorry, we could't save the accommodation!\nAre you sure you are logged in and have internet connection?`;
 const APP_URL = 'travellan://app';
@@ -58,23 +59,27 @@ const Home = () => {
     setIsLoading(true);
 
     if (token && userId && !value) {
-      const data = await ShareExtension.data();
+      try {
+        const data = await ShareExtension.data();
 
-      if (data && data[0].value.match(bookingRegex)) {
-        setValue(data[0].value);
-        const region = data[0].value.split('/')[4];
-        const name = data[0].value.split('/')[5].split('.')[0];
+        if (data && data[0].value.match(bookingRegex)) {
+          setValue(data[0].value);
+          const region = data[0].value.split('/')[4];
+          const name = data[0].value.split('/')[5].split('.')[0];
 
-        try {
-          const result = await axios.get(
-            `${BACKEND_URL}/accommodation/booking?region=${region}&name=${name}`,
-          );
-          setHotel(result.data);
-        } catch {
-          setError(COULD_NOT_SCRAPE_HOTEL_ERROR);
+          try {
+            const result = await axios.get(
+              `${BACKEND_URL}/accommodation/booking?region=${region}&name=${name}`,
+            );
+            setHotel(result.data);
+          } catch {
+            setError(COULD_NOT_SCRAPE_HOTEL_ERROR);
+          }
+        } else {
+          setError(INCORRECT_SHARING_DATA_ERROR);
         }
-      } else {
-        setError(INCORRECT_SHARING_DATA_ERROR);
+      } catch {
+        setError(SHARING_INTENT_ERROR);
       }
     }
   }, [token, userId, value]);
