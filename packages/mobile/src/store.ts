0078c577cf8fd9aea8331b7a4acdb3f2d090abdf
Weronika Olejniczak <wer.olejniczak@gmail.com>
2024-05-04 23:47:09 +0200

    chore(mobile): comment out middleware due to an error

--- packages/mobile/src/store.ts
+++ packages/mobile/src/store.ts
@@ -1,12 +1,19 @@
-import { applyMiddleware, compose, createStore } from 'redux';
+import {
+  applyMiddleware,
+  compose,
+  legacy_createStore as createStore,
+} from 'redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from 'reducers/rootReducer';
 
-const middleware = [thunk];
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+/* const middleware = [thunk];
+const composeEnhancers =
+  (typeof window !== 'undefined' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose; */
 
 export const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(...middleware)),
+  // composeEnhancers(applyMiddleware(...middleware)),
 );
