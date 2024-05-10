4fd02c88144c1ae677322678951b5b513a486377
Weronika Olejniczak <wer.olejniczak@gmail.com>
2024-05-10 20:57:35 +0200

    chore: lint in the pre-commit hook

--- packages/mobile/src/utils/Button.tsx
+++ packages/mobile/src/utils/Button.tsx
@@ -1,7 +1,25 @@
-import React, { memo } from 'react';
+import React, { ReactNode, memo } from 'react';
 import { Button } from 'react-native-paper';
 
 import { Metrics } from 'constants';
+import { ViewStyle } from 'react-native';
+import { IconSource } from 'react-native-paper/lib/typescript/components/Icon';
+
+type PropsType = {
+  children: ReactNode;
+  disabled?: boolean;
+  icon: IconSource;
+  loading?: boolean;
+  mode:
+    | 'text'
+    | 'contained'
+    | 'outlined'
+    | 'elevated'
+    | 'contained-tonal'
+    | undefined;
+  onPress: () => void;
+  style: ViewStyle;
+};
 
 const CustomButton = ({
   children,
@@ -11,12 +29,12 @@ const CustomButton = ({
   mode,
   onPress,
   style,
-}) => (
+}: PropsType) => (
   <Button
     icon={icon}
     loading={loading}
     mode={mode || 'contained'}
-    onPress={disabled ? () => {} : onPress}
+    onPress={disabled ? undefined : onPress}
     style={{ ...Metrics.tinyMargin, ...style }}
   >
     {children}
