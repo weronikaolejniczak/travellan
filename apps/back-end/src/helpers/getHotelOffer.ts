0e69c0cd5edace0b9d4b02c93a40aab0121bbbfb
Weronika Olejniczak <wer.olejniczak@gmail.com>
2024-05-17 21:57:46 +0200

    fix(back-end): fix hotel offer helper args type

--- apps/back-end/src/helpers/getHotelOffer.ts
+++ apps/back-end/src/helpers/getHotelOffer.ts
@@ -1,5 +1,10 @@
-const getHotelOffer = (offers) => {
-  if (Array.isArray(offers) && offers.length > 0) {
+interface IHotelOffer {
+  guests: string;
+  price: string;
+}
+
+const getHotelOffer = (offers: IHotelOffer[]) => {
+  if (Array.isArray(offers) && offers.length > 0 && offers[0]) {
     const guests = offers[0].guests;
     const price = offers[0].price;
 
