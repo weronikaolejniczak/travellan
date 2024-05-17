1039daaae8976b234786f75afca34cee9234e863
Weronika Olejniczak <wer.olejniczak@gmail.com>
2024-05-17 21:57:36 +0200

    fix(back-end): fix location data formatter args type

--- apps/back-end/src/helpers/formatLocationData.ts
+++ apps/back-end/src/helpers/formatLocationData.ts
@@ -1,14 +1,26 @@
-import countryCodes from '../data/countryCodes.json';
+import countryCodes from '../data/country-codes.json';
 import capitalizeEachWord from './capitalizeEachWord';
 
-const formatLocationData = (hotelData) => {
-  const latitude = hotelData.latitude;
-  const longitude = hotelData.longitude;
+interface IHotelData {
+  address: {
+    cityName: string;
+    countryCode: string;
+    lines: string[];
+    postalCode: string;
+  };
+  latitude: string;
+  longitude: string;
+}
 
-  const addressLines = capitalizeEachWord(hotelData.address.lines.join(' '));
-  const postalCode = hotelData.address.postalCode;
-  const cityName = capitalizeEachWord(hotelData.address.cityName);
-  const countryName = countryCodes[hotelData.address.countryCode];
+const formatLocationData = (data: IHotelData) => {
+  const latitude = data.latitude;
+  const longitude = data.longitude;
+
+  const addressLines = capitalizeEachWord(data.address.lines.join(' '));
+  const postalCode = data.address.postalCode;
+  const cityName = capitalizeEachWord(data.address.cityName);
+  const countryName =
+    countryCodes[data.address.countryCode as keyof typeof countryCodes];
 
   const address = `${addressLines}, ${postalCode} ${cityName}, ${countryName}`;
 
