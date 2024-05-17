b13fcaf89ff2cba834d5a918cc78ea63e206d649
Weronika Olejniczak <wer.olejniczak@gmail.com>
2024-05-17 21:57:23 +0200

    fix(back-end): fix credit card payment helper args type

--- apps/back-end/src/helpers/checkIfCreditCardPaymentIsPossible.ts
+++ apps/back-end/src/helpers/checkIfCreditCardPaymentIsPossible.ts
@@ -1,4 +1,14 @@
-const checkIfCreditCardPaymentIsPossible = (data) =>
+interface ICreditCardPaymentData {
+  policies: {
+    guarantee: {
+      acceptedPayments: {
+        methods: string[];
+      };
+    };
+  };
+}
+
+const checkIfCreditCardPaymentIsPossible = (data: ICreditCardPaymentData) =>
   data?.policies?.guarantee?.acceptedPayments?.methods?.includes('CREDIT_CARD');
 
 export default checkIfCreditCardPaymentIsPossible;
