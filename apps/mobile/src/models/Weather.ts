659cca3a26563ecfa4f079e1d4d8c2fa352337ae
Weronika Olejniczak <wer.olejniczak@gmail.com>
2024-05-26 21:28:22 +0200

    refactor(mobile): update Weather model to factory function

--- apps/mobile/src/models/Weather.ts
+++ apps/mobile/src/models/Weather.ts
@@ -1,41 +1,59 @@
-class WeatherModel {
-  constructor(
-    date,
-    sunrise,
-    sunset,
-    maxTemp,
-    minTemp,
-    tempDay,
-    tempNight,
-    tempDayFeelsLike,
-    tempNightFeelsLike,
-    pressure,
-    humidity,
-    windSpeed,
-    cloudiness,
-    description,
-    rain,
-    icon,
-    main,
-  ) {
-    this.date = date;
-    this.sunrise = sunrise;
-    this.sunset = sunset;
-    this.maxTemp = maxTemp;
-    this.minTemp = minTemp;
-    this.tempDay = tempDay;
-    this.tempNight = tempNight;
-    this.tempDayFeelsLike = tempDayFeelsLike;
-    this.tempNightFeelsLike = tempNightFeelsLike;
-    this.pressure = pressure;
-    this.humidity = humidity;
-    this.windSpeed = windSpeed;
-    this.cloudiness = cloudiness;
-    this.description = description;
-    this.rain = rain;
-    this.icon = icon;
-    this.main = main;
-  }
+interface WeatherParams {
+  cloudiness: number;
+  date: Date;
+  description: string;
+  humidity: number;
+  icon: string;
+  main: string;
+  maxTemp: number;
+  minTemp: number;
+  pressure: number;
+  rain: number;
+  sunrise: Date;
+  sunset: Date;
+  tempDay: number;
+  tempDayFeelsLike: number;
+  tempNight: number;
+  tempNightFeelsLike: number;
+  windSpeed: number;
 }
 
-export default WeatherModel;
+const Weather = ({
+  cloudiness,
+  date,
+  description,
+  humidity,
+  icon,
+  main,
+  maxTemp,
+  minTemp,
+  pressure,
+  rain,
+  sunrise,
+  sunset,
+  tempDay,
+  tempDayFeelsLike,
+  tempNight,
+  tempNightFeelsLike,
+  windSpeed,
+}: WeatherParams) => ({
+  cloudiness,
+  date,
+  description,
+  humidity,
+  icon,
+  main,
+  maxTemp,
+  minTemp,
+  pressure,
+  rain,
+  sunrise,
+  sunset,
+  tempDay,
+  tempDayFeelsLike,
+  tempNight,
+  tempNightFeelsLike,
+  windSpeed,
+});
+
+export default Weather;
