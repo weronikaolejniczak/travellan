fe0d8049085297bb65941c36faedc6b39b52f3a3
Weronika Olejniczak <wer.olejniczak@gmail.com>
2024-05-27 22:05:16 +0200

    refactor(mobile): correctly type prepareValue util

--- apps/mobile/src/helpers/prepareValue.ts
+++ apps/mobile/src/helpers/prepareValue.ts
@@ -1,12 +1,12 @@
-const prepareValue = (value) => {
+const prepareValue = (value: string) => {
   value = value.replace(/ /g, '');
   value = parseFloat(value).toFixed(2);
-  value = parseFloat(value);
+  const number = parseFloat(value);
 
-  if (isNaN(value)) {
+  if (isNaN(number)) {
     return 0;
   } else {
-    return value;
+    return number;
   }
 };
 
